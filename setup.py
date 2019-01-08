e738661551d5469f23d5625b19248d2009f30bfe
Jérémy <jrapin.github@gmail.com>
2019-01-08 10:16:08 +0100

    Started updating README (#21)

--- setup.py
+++ setup.py
@@ -4,25 +4,28 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# from distutils.core import setup
 from setuptools import setup
+from setuptools import find_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name='nevergrad',
-    version='0.1.0',
-    description='Gradient-free optimization toolbox',
+    version='0.1.1',
+    license='MIT',
+    description='A Python toolbox for performing gradient-free optimization',
+    long_description=long_description,
     author='Facebook AI Research',
-    packages=[
-        'nevergrad',
-        'nevergrad.benchmark',
-        'nevergrad.benchmark.additional',
-        'nevergrad.common',
-        'nevergrad.functions',
-        'nevergrad.instrumentation',
-        'nevergrad.optimization',
-    ],
+    url="https://github.com/facebookresearch/nevergrad",
+    packages=find_packages(),
+    classifiers=['License :: OSI Approved :: MIT License'
+                 'Intended Audience :: Science/Research',
+                 'Topic :: Scientific/Engineering',
+                 'Programming Language :: Python'],
     install_requires=requirements,
 )
