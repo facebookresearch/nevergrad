c1eadfc29c1e7e040f89ef36bcdd241d5254da91
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2019-01-18 16:02:00 +0000

    Fix build with Python 3
    Traceback (most recent call last):
      File "<string>", line 1, in <module>
      File "setup.py", line 14, in <module>
        long_description = fh.read()
      File "/usr/local/lib/python3.6/encodings/ascii.py", line 26, in decode
        return codecs.ascii_decode(input, self.errors)[0]
    UnicodeDecodeError: 'ascii' codec can't decode byte 0xc2 in position 3837: ordinal not in range(128)
    *** Error code 1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
