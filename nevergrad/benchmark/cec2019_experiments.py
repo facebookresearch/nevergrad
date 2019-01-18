d9a25f7d1e1da05073282320b7092b2eca926ed7
Olivier Teytaud <oteytaud@fb.com>
2019-01-18 17:38:06 +0200

    cec2019

--- nevergrad/benchmark/cec2019_experiments.py
+++ nevergrad/benchmark/cec2019_experiments.py
@@ -15,46 +15,81 @@ from .xpbase import Experiment
 # %% CEC 2019 ## ## ## ## ## ## ## ## ## ## ##
 
 @registry.register
+def deceptivecec(seed: Optional[int] = None) -> Iterator[Experiment]:
+    # prepare list of parameters to sweep for independent variables
+    seedg = create_seed_generator(seed)
+    names = ["deceptivemultimodal", "deceptiveillcond", "deceptivepath"]
+    optims = ["PSO", "MiniQrDE", "MiniLhsDE", "MiniDE", "CMA", "QrDE", "DE", "LhsDE"]
+    optims.append("CustomOptimizer")
+    functions = [ArtificialFunction(name, block_dimension=2, num_blocks=n_blocks, rotation=rotation,
+                                    aggregator=aggregator)
+                 for name in names for rotation in [False, True] for n_blocks in [1, 2, 8, 16] for
+                 aggregator in ["sum", "max"]]
+    for func in functions:
+        for optim in optims:
+            for budget in [25,37,50,75,87] + list(range(100, 3001, 100)):
+                 yield Experiment(func.duplicate(), optim, budget=budget, num_workers=1, seed=next(seedg))
+
+
+@registry.register
+def parallelcec(seed: Optional[int] = None) -> Iterator[Experiment]:
+    # prepare list of parameters to sweep for independent variables
+    seedg = create_seed_generator(seed)
+    names = ["sphere", "rastrigin", "cigar"]
+    optims = ["ScrHammersleySearch", "CMA", "PSO", "NaiveTBPSA", "OnePlusOne", "DE", "TwoPointsDE"]
+    optims.append("CustomOptimizer")
+    functions = [ArtificialFunction(name, block_dimension=bd, useless_variables=bd * uv_factor) for name in names for bd in [25] for uv_factor in [0, 5]]
+    for func in functions:
+        for optim in optims:
+            for budget in [30, 100, 3000]:
+                yield Experiment(func.duplicate(), optim, budget=budget, num_workers=int(budget/5), seed=next(seedg))
+
+
+@registry.register
+def oneshotcec(seed: Optional[int] = None) -> Iterator[Experiment]:
+    seedg = create_seed_generator(seed)
+    names = ["sphere", "rastrigin", "cigar"]
+    optims = sorted(x for x, y in optimization.registry.items() if y.one_shot)
+    optims.append("CustomOptimizer")
+    functions = [ArtificialFunction(name, block_dimension=bd, useless_variables=bd * uv_factor) for name in names for bd in [3, 25] for uv_factor in [0, 5]]
+    for func in functions:
+        for optim in optims:
+            for budget in [30, 100, 3000]:
+                yield Experiment(func.duplicate(), optim, budget=budget, num_workers=budget, seed=next(seedg))
+
+
+@registry.register
+def illcondicec(seed: Optional[int] = None) -> Iterator[Experiment]:
+    """All optimizers on ill cond problems
+    """
+    seedg = create_seed_generator(seed)
+    optims = ["CMA", "PSO", "DE", "MiniDE", "QrDE", "MiniQrDE", "LhsDE", "OnePlusOne", "SQP", "Cobyla", "Powell", "TwoPointsDE", "OnePointDE", "AlmostRotationInvariantDE", "RotationInvariantDE"]
+    optims.append("CustomOptimizer")
+    functions = [ArtificialFunction(name, block_dimension=50,
+                 rotation=rotation) for name in ["cigar", "ellipsoid"]
+                 for rotation in [True, False]]
+    for optim in optims:
+        for function in functions:
+            for budget in [400, 4000, 40000]:
+                yield Experiment(function.duplicate(), optim,
+                    budget=budget, num_workers=1, seed=next(seedg))
+
+
+@registry.register
 def noisycec(seed: Optional[int] = None) -> Iterator[Experiment]:
     """All optimizers on ill cond problems
     """
     seedg = create_seed_generator(seed)
-    optims = ["CustomOptimizer", "TBPSA", "NaiveTBPSA", "CMA", "NoisyBandit", "NoisyDE", "OptimisticNoisyOnePlusOne"]
-    for budget in [500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000]:
+    optims = sorted(x for x, y in optimization.registry.items()
+                    if ("SPSA" in x or "TBPSA" in x or "ois" in x or "epea" in x or "Random" in x))
+    optims.append("CustomOptimizer")
+    for budget in [50000]:
         for optim in optims:
-            for rotation in [True, False]:
-                for name in ["sphere", "cigar", "sphere4"]:
-                    function = ArtificialFunction(name=name, rotation=rotation, block_dimension=20, noise_level=10)
-                    yield Experiment(function, optim, budget=budget, seed=next(seedg))
+          for d in [2, 20, 200]:
+            for rotation in [True]:
+                for name in ["sphere", "rosenbrock"]:
+                    for noise_dissymmetry in [False, True]:
+                        function = ArtificialFunction(name=name, rotation=rotation, block_dimension=d, noise_level=10, noise_dissymmetry=noise_dissymmetry, translation_factor=1.)
+                        yield Experiment(function, optim, budget=budget, seed=next(seedg))
 
 
-@registry.register
-def oneshotcec(seed: Optional[int] = None) -> Iterator[Experiment]:     # 2 variables matter - Scrambled Hammersley rules.
-    # prepare list of parameters to sweep for independent variables
-    seedg = create_seed_generator(seed)
-    optims = ["CustomOptimizer"] + sorted(x for x, y in optimization.registry.items() if y.one_shot and "arg" not in x and "mal" not in x)
-    # functions are not initialized and duplicated at yield time, they will be initialized in the experiment (no need to seed here)
-    for block_dimension in [1, 2, 4]:
-        for useless_coeff in [0, 10]:
-            for optim in optims:
-                for budget in [4, 8, 16, 32, 128, 512, 2048, 8192]:
-                    function = ArtificialFunction("sphere", block_dimension=block_dimension,
-                                                  useless_variables=block_dimension * useless_coeff)
-                    yield Experiment(function, optim, budget=budget, num_workers=budget, seed=next(seedg))
-
-
-@registry.register
-def parallelcec(seed: Optional[int] = None) -> Iterator[Experiment]:     # 2 variables matter - Scrambled Hammersley rules.
-    # prepare list of parameters to sweep for independent variables
-    seedg = create_seed_generator(seed)
-    optims = ["CustomOptimizer", "TwoPointsDE", "RandomSearch", "TBPSA", "CMA", "NaiveTBPSA", "PortfolioNoisyDiscreteOnePlusOne"]
-    # functions are not initialized and duplicated at yield time, they will be initialized in the experiment (no need to seed here)
-    for budget in [4, 8, 16, 32, 128, 512, 2048, 8192]:
-        for block_dimension in [1, 2, 4]:
-            for useless_coeff in [0, 10]:
-                for optim in optims:
-                    for name in ["sphere"]:
-                        for num_workers in [4, 16, 64]:
-                            function = ArtificialFunction(name, block_dimension=block_dimension,
-                                                          useless_variables=block_dimension * useless_coeff)
-                            yield Experiment(function, optim, budget=budget, num_workers=num_workers, seed=next(seedg))
