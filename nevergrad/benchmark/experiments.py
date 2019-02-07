bf4eb22e3a555b453c45f22c2983b9ac24dfb6e4
Olivier Teytaud <oteytaud@fb.com>
2019-02-07 14:47:31 +0200

    fixingxps

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -172,23 +172,41 @@ def spsa_benchmark(seed: Optional[int] = None) -> Iterator[Experiment]:
 @registry.register
 def mlda(seed: Optional[int] = None) -> Iterator[Experiment]:
     funcs: List[BaseFunction] = [_mlda.Clustering.from_mlda(name, num, rescale)
-                                 for name, num in [("Ruspini", 5), ("German towns", 10)] for rescale in [False]]  # I removed True
+                                 for name, num in [("Ruspini", 5), ("German towns", 10)] for rescale in [True, False]]
     funcs += [_mlda.SammonMapping.from_mlda("Virus", rescale=False), _mlda.SammonMapping.from_mlda("Virus", rescale=True),
               _mlda.SammonMapping.from_mlda("Employees")]
-    funcs += [_mlda.Perceptron.from_mlda(name) for name in ["sine"]]  #"quadratic", "sine", "abs", "heaviside"]]
+    funcs += [_mlda.Perceptron.from_mlda(name) for name in ["quadratic", "sine", "abs", "heaviside"]]
     funcs += [_mlda.Landscape(transform) for transform in [None, "square", "gaussian"]]
     seedg = create_seed_generator(seed)
-    #algos = ["NaiveTBPSA", "SQP", "Powell", "LargeScrHammersleySearch", "ScrHammersleySearch",
+    algos = ["NaiveTBPSA", "SQP", "Powell", "LargeScrHammersleySearch", "ScrHammersleySearch",
+    for budget in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400, 12800]:
+        for num_workers in [10, 100, 1000]:  #[1, 10, 100]:
+            if num_workers < budget:
+                for algo in algos:
+                    for func in funcs:
+                        xp = Experiment(func, algo, budget, num_workers=num_workers, seed=next(seedg))
+                        if not xp.is_incoherent:
+                            yield xp
+
+
+@registry.register
+def mldaas(seed: Optional[int] = None) -> Iterator[Experiment]:
+    funcs: List[BaseFunction] = [_mlda.Clustering.from_mlda(name, num, rescale)
+                                 for name, num in [("Ruspini", 5), ("German towns", 10)] for rescale in [True, False]]
+    funcs += [_mlda.SammonMapping.from_mlda("Virus", rescale=False), _mlda.SammonMapping.from_mlda("Virus", rescale=True),
+              _mlda.SammonMapping.from_mlda("Employees")]
+    funcs += [_mlda.Perceptron.from_mlda(name) for name in ["quadratic", "sine", "abs", "heaviside"]]
+    funcs += [_mlda.Landscape(transform) for transform in [None, "square", "gaussian"]]
+    seedg = create_seed_generator(seed)
     algos = ["NaiveTBPSA", "ScrHammersleySearch",
-             "PSO", "OnePlusOne", "CMA", "OnePointDE", "TwoPointsDE", "QrDE", "LhsDE", "Zero",   # Cobyla freezes :(
-              "PortfolioDiscreteOnePlusOne", "CauchyOnePlusOne", "RandomSearch", "RandomSearchPlusMiddlePoint", "HaltonSearchPlusMiddlePoint", "MiniQrDE","HaltonSearch", "RandomScaleRandomSearch", "MiniDE", "DiscreteOnePlusOne",
-             "ScrHaltonSearch", "ScrHammersleySearchPlusMiddlePoint", "HaltonSearch", "MilliCMA", "MicroCMA"]
-#    algos = ["ASelect2", "CMA", "ScrHammersleySearch", "TwoPointsDE"]
+             "PSO", "OnePlusOne", "CMA", "OnePointDE", "TwoPointsDE", "QrDE", "LhsDE", "Zero",
+             "PortfolioDiscreteOnePlusOne", "CauchyOnePlusOne", "RandomSearch",
+             "RandomSearchPlusMiddlePoint", "HaltonSearchPlusMiddlePoint", "MiniQrDE",
+             "HaltonSearch", "RandomScaleRandomSearch", "MiniDE", "DiscreteOnePlusOne",
+             "ScrHaltonSearch", "ScrHammersleySearchPlusMiddlePoint", "HaltonSearch",
+             "MilliCMA", "MicroCMA"]
     # pylint: disable=too-many-nested-blocks
-    algos += ["CMADECMA", "AS6", "AS10", "AShalf",  "AS2", "AS", "Portfolio", "ASelect", "ASelect2", "ASelect3", "ASelect4", "ASelect5", "MultiCMA", "TripleCMA", "MultiScaleCMA"]
-    algos += ["MAS"]
-    algos += ["MAS2", "CM"]
-    algos += ["CM2","CM3"]
+    algos += ["MultiCMA", "TripleCMA", "MultiScaleCMA", "CM2","CM3", "AS2", "CMADE", "AS", "Portfolio"]
     for budget in [9600, 12800, 25600]:#, 51200]:#, 102400]:
         for num_workers in [10, 100, 1000]:  #[1, 10, 100]:
             for algo in algos:
