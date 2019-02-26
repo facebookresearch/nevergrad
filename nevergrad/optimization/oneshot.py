70052e50add51ad193aff92f5e238c4fa9fe4ff0
Jeremy Rapin <jrapin@fb.com>
2019-02-26 09:17:10 +0100

    Correct budget for middle point

--- nevergrad/optimization/oneshot.py
+++ nevergrad/optimization/oneshot.py
@@ -99,7 +99,7 @@ class _DetermisticSearch(OneShotOptimizer):
     @property
     def sampler(self) -> sequences.Sampler:
         if self._sampler_instance is None:
-            budget = None if self.budget is None else self.budget  # TODO: - self._middle_point
+            budget = None if self.budget is None else self.budget - self._parameters.middle_point
             samplers = {"Halton": sequences.HaltonSampler,
                         "Hammersley": sequences.HammersleySampler,
                         "LHS": sequences.LHSSampler,
