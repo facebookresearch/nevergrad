c106010594550d4f5b9593b8f36b20876fe76735
Jeremy Rapin <jrapin@fb.com>
2019-05-06 16:05:36 +0200

    Add a header

--- nevergrad/optimization/requirements_check.py
+++ nevergrad/optimization/requirements_check.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from pathlib import Path
 from nevergrad import optimization
 from nevergrad import instrumentation as inst
