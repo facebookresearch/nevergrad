c1f0e8da6d427b8ee584d4d2ff1c4e847ca3826e
Jeremy Rapin <jrapin@fb.com>
2019-05-20 15:44:29 +0200

    Improve nevergrad namespace

--- nevergrad/__init__.py
+++ nevergrad/__init__.py
@@ -3,4 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from .optimization import optimizerlib as lib
+from .optimization.optimizerlib import registry as optimizers
+from .instrumentation import Instrumentation
+from .instrumentation import variables as var
+
 __version__ = "0.2.1"
