1009878a43c9340f6fc771acb688340fe2858c9c
Teytaud <oteytaud@fb.com>
2019-07-24 15:46:37 +0300

    Update experiments.py

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -23,12 +23,35 @@ from . import frozenexperiments  # noqa # pylint: disable=unused-import
 
 
 @registry.register
+def discrete2(seed: Optional[int] = None) -> Iterator[Experiment]:
+    # prepare list of parameters to sweep for independent variables
+    seedg = create_seed_generator(seed)
+    names = [n for n in ArtificialFunction.list_sorted_function_names() if ("one" in n or "jump" in n) and not "5" in n]
+    optims = sorted(
+        x for x, y in ng.optimizers.registry.items() if "andomSearch" in x or "PBIL" in x or "cGA" in x or ("iscrete" in x and "epea" not in x and "DE" not in x and "SSNEA" not in x)
+    )
+    functions = [
+        ArtificialFunction(name, block_dimension=bd, num_blocks=n_blocks, useless_variables=bd * uv_factor * n_blocks)
+        for name in names
+        for bd in [30]
+        for uv_factor in [0, 5, 10]
+        for n_blocks in [1]
+    ]
+    # functions are not initialized and duplicated at yield time, they will be initialized in the experiment (no need to seed here)
+    for func in functions:
+        for optim in optims:
+            for budget in [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700,
+                           1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000]:  # , 10000]:
+                yield Experiment(func.duplicate(), optim, budget=budget, num_workers=1, seed=next(seedg))
+
+
+@registry.register
 def discrete(seed: Optional[int] = None) -> Iterator[Experiment]:
     # prepare list of parameters to sweep for independent variables
     seedg = create_seed_generator(seed)
     names = [n for n in ArtificialFunction.list_sorted_function_names() if "one" in n or "jump" in n]
     optims = sorted(
-        x for x, y in ng.optimizers.registry.items() if "PBIL" in x or "cGA" in x or ("iscrete" in x and "epea" not in x and "DE" not in x and "SSNEA" not in x)
+        x for x, y in ng.optimizers.registry.items() if "andomSearch" in x or ("iscrete" in x and "epea" not in x and "DE" not in x and "SSNEA" not in x)
     )
     functions = [
         ArtificialFunction(name, block_dimension=bd, num_blocks=n_blocks, useless_variables=bd * uv_factor * n_blocks)
