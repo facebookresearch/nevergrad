3a63baad10d818fe92df4adff1967aa26927bf2a
Teytaud <oteytaud@fb.com>
2019-09-24 17:06:25 +0200

    Update battleship.py

--- nevergrad/games/battleship.py
+++ nevergrad/games/battleship.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 #
+# We have two versions of the game:
 # version 1: oteytaud's version, simple.
 # version 2: version as in https://stackoverflow.com/questions/1631414/what-is-the-best-battleship-ai
 #        also consistent with https://github.com/Dbz/Battleship/blob/master/BattleShip.py
@@ -40,7 +41,8 @@ class battleship(object):
             board += [[' '] * N]
         self.N = N
         self.board = board
-# self.ship_length = []  # for dbz
+        self.short_policy = 100
+        self.long_policy = 5000
 
     def __str__(self):
         serialized = "".join(["".join(b) for b in self.board])
@@ -64,153 +66,42 @@ class battleship(object):
         s = random.choice(possible_shoots)
         self.shoot(s[0], s[1])
 
-# def dbz_shoot(self):
-# if not len(self.ship_length):
-####          x = random.choice(range(self.boardsize()))
-####          y = random.choice(range(self.boardsize()))
-# while self.board[x][y] in ["x", "*"]:
-####            x = random.choice(range(self.boardsize()))
-####            y = random.choice(range(self.boardsize()))
-####          self.shoot(x, y)
-# if self.board[x][y] == 'x':
-####
-# return
-####
-####
-# ============================
-# if not len(ship_length): # No current targets
-# if not is_ocean(ai_guess_row, ai_guess_col, player_board): # AI hit
-####                miss = 0
-####                player_alive -= 1
-# print "Hit ship length: ", ship_number(ai_guess_row, ai_guess_col)
-####                ship_length.append((ship_number(ai_guess_row, ai_guess_col)))
-# print "ship_position length: ", str(len(ship_position))
-####                ship_position.extend([ai_guess_row, ai_guess_col])
-# print "ship_position length: ", str(len(ship_position))
-####                orientation = -1
-####                player_board[ai_guess_row][ai_guess_col] = HIT
-####                ai_radar[ai_guess_row][ai_guess_col] = HIT
-# total_hits.append(number_board[ai_guess_row][ai_guess_col])
-####                print ("Attenton Admiral! You have been hit!")
-# else:
-####                miss = 1
-####                player_board[ai_guess_row][ai_guess_col] = FIRE
-####                ai_radar[ai_guess_row][ai_guess_col] = FIRE
-####                print ("Good news! They've missed!")
-# else: # Find next spot to shoot on ship
-# print "Current Targets: ", " ".join(map(str, ship_length)),":", " ".join(map(str,total_hits))
-# print "Last shot was a miss: ", miss
-# if orientation == -1: # shot-test for orientation of hit ship
-# ship_position[ swapped for ai_hit_
-####                print "Ship has no orientation"
-# if is_ocean(ship_position[0]+1, ship_position[1], ai_radar):
-####                    ai_guess_row = ship_position[0]+1
-####                    ai_guess_col = ship_position[1]
-# elif is_ocean(ship_position[0]-1, ship_position[1], ai_radar):
-####                    ai_guess_row = ship_position[0]-1
-####                    ai_guess_col = ship_position[1]
-# elif is_ocean(ship_position[0], ship_position[1]-1, ai_radar):
-####                    ai_guess_row = ship_position[0]
-####                    ai_guess_col = ship_position[1]-1
-# else:
-####                    ai_guess_row = ship_position[0]
-####                    ai_guess_col = ship_position[1]+1
-# elif orientation: # Shoot at verticle ship
-# print "Previous Guess: ", ai_guess_row, ":", ai_guess_col
-# for item in ai_radar:
-####                    print item[0], ' '.join(map(str, item[1:]))
-# if is_ocean(ai_guess_row+1, ai_guess_col, ai_radar) and not miss:
-####                    ai_guess_row += 1
-# else:
-# print "Adjusting guess to lower row number"
-####                    ai_guess_row -= 1
-####
-# while not is_ocean(ai_guess_row, ai_guess_col, ai_radar): # not is important here
-####                        ai_guess_row -= 1
-# print "New Guess: ", ai_guess_row, ":", ai_guess_col
-# else: # Shoot at horizontal ship
-# print "Previous Guess: ", ai_guess_row, ":", ai_guess_col
-# for item in ai_radar:
-####                    print item[0], ' '.join(map(str, item[1:]))
-# if is_ocean(ai_guess_row, ai_guess_col-1, ai_radar) and not miss:
-####                    ai_guess_col = ai_guess_col-1
-# else:
-# print "Adjusting guess to higher col number"
-####                    ai_guess_col = ai_guess_col+1
-# while not is_ocean(ai_guess_row, ai_guess_col, ai_radar):
-####                        ai_guess_col += 1
-# print "New Guess: ", ai_guess_row, ":", ai_guess_col
-# Set boards after shots
-# if not is_ocean(ai_guess_row, ai_guess_col, player_board):
-####
-# number_board[ai_guess_row][ai_guess_col] = OCEAN
-# print "Setting Board: ", ai_guess_row, ":", ai_guess_col
-####                player_board[ai_guess_row][ai_guess_col] = HIT
-####                ai_radar[ai_guess_row][ai_guess_col] = HIT
-# total_hits.append(number_board[ai_guess_row][ai_guess_col])
-# ship_position.extend([ai_guess_row, ai_guess_col])
-####                player_alive -= 1
-####
-# if second_shot: # set orientation
-# print "DEBUG: ", total_hits.count(total_hits[0]), ship_number(ai_guess_row, ai_guess_col), ship_number(ship_position[0], ship_position[1])
-# if total_hits.count(total_hits[0]) == 2 and ship_number(ai_guess_row, ai_guess_col) == ship_number(ship_position[0], ship_position[1]):
-# if ai_guess_col != ship_position[1]:
-####                        orientation = 0
-# else:
-####                        orientation = 1
-####                    print "New Orientation: ", orientation
-# elif total_hits[0] != number_board[ai_guess_row][ai_guess_col]: # Other ship was shot
-####                    ship_length.append((ship_number(ai_guess_row, ai_guess_col)))
-####                    ship_position.extend([ai_guess_row, ai_guess_col])
-# if player_alive:
-####                    miss = 0
-####                    print ("Attenton Admiral! You have been hit!")
-# else:
-####                    print ("I'm sorry sir, but we're going down")
-# print_board()
-# break
-# else: # AI missed
-# print "DEBUG: r,c: ", ai_guess_row, ", ", ai_guess_col
-####                miss = 1
-####                player_board[ai_guess_row][ai_guess_col] = FIRE
-####                ai_radar[ai_guess_row][ai_guess_col] = FIRE
-####                print ("Good news! They've missed!")
-# if ship_sunk(): # Reset variables
-# print "Ship sunk"
-####                    orientation = -1
-# ship_position.pop(0)
-# ship_position.pop(0)
-# ship_length.pop(0)
-####
-####                    t = total_hits[0]
-# for x in range(total_hits.count(t)):
-# total_hits.remove(t)
-####
-# print "Targets after sinking: ", " ".join(map(str, ship_length)),":", " ".join(map(str,total_hits))
-# if len(ship_length) != 0:
-####                        miss = 0
-# else:
-####                        miss = 1
-# print "ship_position list: ", " ".join(map(str, ship_position))
-# =======================
-# TODO
-
     def good_shoot(self, policy):
         random.seed()
         if policy is not None:
-            random.seed(random.choice([policy[i] for i in range(20, 40)]))
+            index = 0
+            random.seed(17)
+            for i in range(self.N):
+                for j in range(self.N):
+                    if board[i][j] == "o":
+                        index *= random.choice(30) + 1
+                        index += random.choice(range(30))
+                        index = index % 10000000
+                    if board[i][j] == "x":
+                        index *= random.choice(30) + 1
+                        index += random.choice(range(30, 60))
+                        index = index % 10000000
+                    if board[i][j] == "*":
+                        index *= random.choice(30) + 1
+                        index += random.choice(range(60, 90))
+                        index = index % 10000000
+                    if board[i][j] == " ":
+                        index *= random.choice(30) + 1
+                        index += random.choice(range(90, 120))
+                        index = index % 10000000            
+            random.seed(policy[index % len(policy)])
         possible_shoots = defaultdict(lambda: 0)
         for _ in range(7):
             candidate_board = self.compliant_board()
-#      print "a candidate board for "
-#      print self
-#      print " is "
-#      print candidate_board
+            #      print "a candidate board for "
+            #      print self
+            #      print " is "
+            #      print candidate_board
             for i in range(self.N):
                 for j in range(self.N):
                     if candidate_board.board[i][j] == "o" and self.board[i][j] != "x":
                         possible_shoots[(i, j)] += 1
-#    print possible_shoots.keys()
+        #    print possible_shoots.keys()
         max_num = -1
         assert possible_shoots, str(self) + " has no possible shoots."
         for s in possible_shoots:
@@ -365,7 +256,7 @@ class battleship(object):
     def fill(self, s=None):
         random.seed()
         if s is not None:
-            s = random.choice([s[i] for i in range(20)])
+            s = random.choice([s[i] for i in range(self.short_policy)])
             random.seed(s)
         N = self.N
         for size in self.sizes():
@@ -406,18 +297,18 @@ class battleship(object):
             return True
 
 
-def test():
-    b = battleship()
-    b.fill()
-    for _ in range(3):
-        b.shoot(random.randint(0, b.N-1), random.randint(0, b.N-1))
-    c = b.compliant_board()
-    print(b, c)
+# def test():
+#     b = battleship()
+#     b.fill()
+#     for _ in range(3):
+#         b.shoot(random.randint(0, b.N-1), random.randint(0, b.N-1))
+#     c = b.compliant_board()
+#     print(b, c)
 
 
 def play_game(policy1=None, policy2=None, version=1):
     if policy1 is None and policy2 is None:
-        return 40
+        return self.long_policy
     board1 = battleship(version=version)
     board2 = battleship(version=version)
     board1.fill(policy1)
