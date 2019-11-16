1c7c523e2939085a8d5d7e71982aaf2190f5aa6b
Jeremy Rapin <jrapin@fb.com>
2019-11-15 18:55:44 -0800

    Feature test

--- nevergrad/instrumentation/parameter.py
+++ nevergrad/instrumentation/parameter.py
@@ -17,15 +17,11 @@ class Array(Parameter):
     def value(self) -> np.ndarray:
         return self._value
 
-    @value.setter
-    def value(self, new_value: np.ndarray) -> None:
-        if not isinstance(new_value, np.ndarray):
-            raise TypeError(f"Received a {type(new_value)} in place of a np.ndarray")
-        if self._value.shape != new_value.shape:
-            raise ValueError(f"Cannot set array of shape {self._value.shape} with value of shape {new_value.shape}")
-        self._value = new_value
-
     def with_value(self, value: np.ndarray) -> None:
+        if not isinstance(value, np.ndarray):
+            raise TypeError(f"Received a {type(value)} in place of a np.ndarray")
+        if self._value.shape != value.shape:
+            raise ValueError(f"Cannot set array of shape {self._value.shape} with value of shape {value.shape}")
         self._value = value
 
     def with_std_data(self, data: np.ndarray, deterministic: bool = True) -> None:
