2311746aa4f5fa61214fbf5809522c5d6fe53afb
Teytaud <oteytaud@fb.com>
2019-11-20 09:45:55 +0100

    Update optimizerlib.py

--- nevergrad/optimization/optimizerlib.py
+++ nevergrad/optimization/optimizerlib.py
@@ -805,8 +805,9 @@ class Splitter(base.Optimizer):
         self.instrumentations: List[Any] = []
         self.num_yoyo = 0
         for i in range(self.num_optims):
-            if not self.num_vars or len(self.num_vars) < i:
+            if not self.num_vars or len(self.num_vars) < i+1:
                 self.num_vars += [(self.dimension // self.num_optims) + (self.dimension % self.num_optims > i)]
+            
             assert len(self.num_vars) > 0
             assert self.num_vars[-1] >= 0
             self.instrumentations += [Instrumentation(inst.variables.Array(self.num_vars[i]).affined(1, 0))]
