64d34e04e13443455c9b991772eb73ffec4e6e37
Olivier Teytaud <oteytaud@fb.com>
2019-11-28 10:07:34 +0100

    start

--- autorun.sh
+++ autorun.sh
@@ -3,5 +3,5 @@
 listxp=`grep -i1 "^def" nevergrad/benchmark/*experiments.py | grep -i1 '@regis' | grep ':def' | sed 's/.*:def //g' | sed 's/(.*//g'`
 for xp in $listxp
 do
-	echo "Experiment $i ==============================="
+	echo "Experiment $xp ==============================="
 done
