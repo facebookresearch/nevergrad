0de5d3511be40f52b4083387e9159e65c71a2c19
Olivier Teytaud <oteytaud@fb.com>
2019-12-03 06:52:30 +0100

    fix

--- nevergrad/examples/powersystem.py
+++ nevergrad/examples/powersystem.py
@@ -22,7 +22,7 @@ for num_dams in [3,6,12]:
 #     failure_cost: float = 500.,  # Cost of not satisfying the demand. Equivalent to an expensive infinite capacity thermal plant.
 
                     power_system_loss = PowerSystem(num_dams=num_dams, depth=depth, width=width, year_to_day_ratio=year_to_day_ratio, back_to_normal=back_to_normal, num_thermal_plants=num_thermal_plants, constant_to_year_ratio=constant_to_year_ratio) 
-                    optimizer = optimizerlib.SplitOptimizer3(instrumentation=power_system_loss.dimension, budget=60, num_workers=10) 
+                    optimizer = optimizerlib.SplitOptimizer9(instrumentation=power_system_loss.dimension, budget=500, num_workers=10) 
                     optimizer.minimize(power_system_loss)
                     power_system_loss(optimizer.provide_recommendation().data)
                     power_system_loss.make_plots(f"ps_{num_dams}dams_{depth}_{width}_ytdr{year_to_day_ratio}_btn{back_to_normal}_num_thermal_plants{num_thermal_plants}_ctyr{constant_to_year_ratio}.png")
