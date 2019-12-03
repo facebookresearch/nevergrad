f298363010cae77e0ed034f78658f6b89e664031
Olivier Teytaud <oteytaud@fb.com>
2019-12-03 11:56:08 +0200

    autorun

--- autorun.sh
+++ autorun.sh
@@ -14,6 +14,7 @@ do
     # Quite small scale for the moment. More repetitions and more workers later, when it will be clear that everything
     # is fine.
     # Using learnfair queue is risky; this will also be discussed
+    rm -rf outputs/${xp}
     python -m dfoptim.benchmark.slurm $xp --seed=1 --repetitions=7 --num_workers=111 --partition=learnfair  &
     sleep $T
     popd
@@ -23,29 +24,4 @@ sleep $T
 sleep $T
 sleep $T
 sleep $T
-echo '<html><head><title>nevergrad xps</title></head><body>' > allxps/list.html
-for xp in $listxp
-do
-    echo "<br> $xp </br><p>" >> allxps/list.html
-    pushd ..
-    python -m dfoptim.benchmark.slurmplot outputs/$xp --max_combsize=2 
-    # FIXME add --competencemaps=True
-    mkdir -p nevergrad_repository/allxps/${xp}
-    tar -xzf outputs/${xp}.tar.gz ./data.csv
-    tar --wildcards -xzf outputs/${xp}.tar.gz ./fight_*.png
-    mv fight*.png data.csv nevergrad_repository/allxps/${xp}/
-    gzip nevergrad_repository/allxps/${xp}/*.csv
-    popd
-    echo "<h1> ${xp} <\/h1>" >> allxps/list.html
-    ls allxps/${xp}/fight_all.png | sed 's/.*/<img src="https:\/\/dl.fbaipublicfiles.com\/nevergrad\/&">&<p>/g' >> allxps/list.html
-    ls allxps/${xp}/*.png | grep -v fight_all | sed 's/.*/<a href="https:\/\/dl.fbaipublicfiles.com\/nevergrad\/&">&<\/a>/g' >> allxps/list.html
-    ls allxps/${xp}/*.csv* | sed 's/.*/<a href="https:\/\/dl.fbaipublicfiles.com\/nevergrad\/&">&<\/a>/g' >> allxps/list.html
-done
-echo '</body></html>' >> allxps/list.html
-fs3cmd sync allxps/** s3://dl.fbaipublicfiles.com/nevergrad/allxps/
-echo 'https://dl.fbaipublicfiles.com/nevergrad/allxps/list.html'
-
-
-
-
-
+./autorun_plot.sh
---
+++ autorun_plot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+
+# FIXME: Should we include the module load here ?
+listxp=`grep -i1 "^def" nevergrad/benchmark/*experiments.py | grep -i1 '@regis' | grep ':def' | sed 's/.*:def //g' | sed 's/(.*//g'`
+
+touch allxps
+rm -rf allxps
+echo '<html><head><title>nevergrad xps</title></head><body>' > allxps/list.html
+for xp in $listxp
+do
+    echo "<br> $xp </br><p>" >> allxps/list.html
+    pushd ..
+    python -m dfoptim.benchmark.slurmplot outputs/$xp --max_combsize=2 
+    # FIXME add --competencemaps=True
+    mkdir -p nevergrad_repository/allxps/${xp}
+    tar -xzf outputs/${xp}.tar.gz ./data.csv
+    tar --wildcards -xzf outputs/${xp}.tar.gz ./fight_*.png
+    mv fight*.png data.csv nevergrad_repository/allxps/${xp}/
+    gzip nevergrad_repository/allxps/${xp}/*.csv
+    popd
+    echo "<h1> ${xp} </h1>" >> allxps/list.html
+    ls allxps/${xp}/fight_all.png | sed 's/.*/<img src="https:\/\/dl.fbaipublicfiles.com\/nevergrad\/&">&<p>/g' >> allxps/list.html
+    echo '<font size="-6">' >> allxps/list.html
+    ls allxps/${xp}/*.png | grep -v fight_all | sed 's/.*/<a href="https:\/\/dl.fbaipublicfiles.com\/nevergrad\/&">&<\/a>/g' >> allxps/list.html
+    ls allxps/${xp}/*.csv* | sed 's/.*/<a href="https:\/\/dl.fbaipublicfiles.com\/nevergrad\/&">&<\/a>/g' >> allxps/list.html
+    echo '</font>' >> allxps/list.html
+done
+echo '</body></html>' >> allxps/list.html
+fs3cmd sync allxps/** s3://dl.fbaipublicfiles.com/nevergrad/allxps/
+echo 'https://dl.fbaipublicfiles.com/nevergrad/allxps/list.html'
+
+
+
+
+
