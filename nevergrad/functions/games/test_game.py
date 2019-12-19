bd03841fe7925fa82cdaccccecc2b3b0da192e96
Olivier Teytaud <oteytaud@fb.com>
2019-12-19 17:24:50 +0100

    fix

--- nevergrad/functions/games/test_game.py
+++ nevergrad/functions/games/test_game.py
@@ -1,15 +1,15 @@
-
+from ...common import testing
 from typing import Any
 from typing import List
 import numpy as np
 from . import game
 
-@pytest.mark.parametrized("name", ["war", "flip", "batawaf", "guesswho", "bigguesswho"])
-def test_games(name: str) -> None
-    dimension = game._Game().play_game(g)
+@testing.parametrized(**{name: (name,) for name in ["war", "flip", "batawaf", "guesswho", "bigguesswho"]})
+def test_games(name: str) -> None:
+    dimension = game._Game().play_game(name)
     res: List[Any] = []
     for _ in range(200):
-        res += [game._Game.play_game(g, np.random.uniform(0, 1, dimension), None)]
+        res += [game._Game.play_game(name, np.random.uniform(0, 1, dimension), None)]
         score = (float(sum(1 if r == 2 else 0 if r == 1 else 0.5 for r in res)) / len(res))
     assert score >= 0.1
     assert score <= 0.9
