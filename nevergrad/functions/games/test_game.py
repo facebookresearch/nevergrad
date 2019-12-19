14c45737f2fe7b4991c0f21cc7d216e25a17d299
Olivier Teytaud <oteytaud@fb.com>
2019-12-19 17:26:02 +0100

    fix

--- nevergrad/functions/games/test_game.py
+++ nevergrad/functions/games/test_game.py
@@ -9,7 +9,7 @@ def test_games(name: str) -> None:
     dimension = game._Game().play_game(name)
     res: List[Any] = []
     for _ in range(200):
-        res += [game._Game.play_game(name, np.random.uniform(0, 1, dimension), None)]
+        res += [game._Game().play_game(name, np.random.uniform(0, 1, dimension), None)]
         score = (float(sum(1 if r == 2 else 0 if r == 1 else 0.5 for r in res)) / len(res))
     assert score >= 0.1
     assert score <= 0.9
