4b6ee3298112e4cf037425e9d984b5264e6ed650
Olivier Teytaud <oteytaud@fb.com>
2019-12-19 15:01:20 +0100

    bad

--- nevergrad/functions/games/game.py
+++ nevergrad/functions/games/game.py
@@ -423,8 +423,7 @@ class Game(inst.InstrumentedFunction):
         # FIXME: an adaptive opponent, e.g. bandit, would be better.
         # We play a game as player 1.
         np_state = np.random.get_state()
-        np.random.seed(self.instrumentation.random_state.randint(2**32, dtype=np.uint32))
-        np.random.seed(3)
+        np.random.seed(self.instrumentation.random_state.randint(12560, dtype=np.uint32))
         p1 = x[:(self.dimension // 2)]
         p2 = self.instrumentation.random_state.normal(size=self.dimension // 2)
         r = self.game_object.play_game(self.game, p1, p2)
