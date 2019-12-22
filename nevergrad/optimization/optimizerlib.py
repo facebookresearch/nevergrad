7211dd2693ab29797c78f4f797b22ec9438dd554
Fabien Teytaud <teytaud@univ-littoral.fr>
2019-12-22 13:23:46 +0100

    correction of all eda based algo

--- nevergrad/optimization/optimizerlib.py
+++ nevergrad/optimization/optimizerlib.py
@@ -21,7 +21,6 @@ from .base import registry as registry
 from .base import addCompare
 from .base import InefficientSettingsWarning
 from . import sequences
-import sys
 
 # families of optimizers
 # pylint: disable=unused-wildcard-import,wildcard-import, too-many-lines
@@ -309,7 +308,6 @@ class EDA(base.Optimizer):
             # Sorting the population.
             sorted_pop_with_sigma_and_fitness = [
                 (i, s, f)
-                # for f, i, s in sorted(zip(self.evaluated_population_fitness, self.evaluated_population, self.evaluated_population_sigma))
                 for f, i, s in sorted(zip(self.evaluated_population_fitness, self.evaluated_population, self.evaluated_population_sigma), key = lambda t: t[0])
             ]
             self.evaluated_population = [p[0] for p in sorted_pop_with_sigma_and_fitness]
@@ -369,7 +367,7 @@ class PCEDA(EDA):
             # Sorting the population.
             sorted_pop_with_sigma_and_fitness = [
                 (i, s, f)
-                for f, i, s in sorted(zip(self.evaluated_population_fitness, self.evaluated_population, self.evaluated_population_sigma))
+                for f, i, s in sorted(zip(self.evaluated_population_fitness, self.evaluated_population, self.evaluated_population_sigma), key = lambda t: t[0])
             ]
             self.evaluated_population = [p[0] for p in sorted_pop_with_sigma_and_fitness]
             self.covariance = np.cov(np.array(self.evaluated_population).T)
@@ -425,7 +423,7 @@ class MPCEDA(EDA):
             # Sorting the population.
             sorted_pop_with_sigma_and_fitness = [
                 (i, s, f)
-                for f, i, s in sorted(zip(self.evaluated_population_fitness, self.evaluated_population, self.evaluated_population_sigma))
+                for f, i, s in sorted(zip(self.evaluated_population_fitness, self.evaluated_population, self.evaluated_population_sigma), key = lambda t: t[0])
             ]
             self.evaluated_population = [p[0] for p in sorted_pop_with_sigma_and_fitness]
             self.covariance *= 0.9
@@ -462,7 +460,7 @@ class MEDA(EDA):
             # Sorting the population.
             sorted_pop_with_sigma_and_fitness = [
                 (i, s, f)
-                for f, i, s in sorted(zip(self.evaluated_population_fitness, self.evaluated_population, self.evaluated_population_sigma))
+                for f, i, s in sorted(zip(self.evaluated_population_fitness, self.evaluated_population, self.evaluated_population_sigma), key = lambda t: t[0])
             ]
             self.evaluated_population = [p[0] for p in sorted_pop_with_sigma_and_fitness]
             self.covariance *= 0.9
