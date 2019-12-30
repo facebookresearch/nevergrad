e45cbf77e0c4fd3c199e9e3f33a8d4b5eeea119a
Jeremy Rapin <jrapin@fb.com>
2019-12-30 11:35:09 +0100

    Revert and clean power system code

--- nevergrad/functions/games/game.py
+++ nevergrad/functions/games/game.py
@@ -401,7 +401,7 @@ class Game(inst.InstrumentedFunction):  # TODO: Improve seeding support (with Pa
     def _simulate_game(self, x: np.ndarray) -> float:
         # FIXME: an adaptive opponent, e.g. bandit, would be better.
         # We play a game as player 1.
-        np_state = np.random.get_state()  # TODO avoid this (wont support multiprocessing)
+        # np_state = np.random.get_state()  # TODO TOO DANGEREOUS! this can make the game play the same move all over again
         p1 = x[:(self.dimension // 2)]
         p2 = np.random.normal(size=self.dimension // 2)
         r = self.game_object.play_game(self.game, p1, p2)
@@ -410,5 +410,5 @@ class Game(inst.InstrumentedFunction):  # TODO: Improve seeding support (with Pa
         p1 = np.random.normal(size=self.dimension // 2)
         p2 = x[(self.dimension // 2):]
         r = self.game_object.play_game(self.game, p1, p2)
-        np.random.set_state(np_state)
+        # np.random.set_state(np_state)
         return (result + (0. if r == 2 else 0.5 if r == 0 else 1.)) / 2
