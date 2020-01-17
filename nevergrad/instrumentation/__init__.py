7cf9dd850867d786ddc637c3645337eafc5ce557
Jeremy Rapin <jrapin@fb.com>
2020-01-17 20:46:52 +0100

    Solve tests and deprecate import

--- nevergrad/instrumentation/__init__.py
+++ nevergrad/instrumentation/__init__.py
@@ -3,11 +3,18 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import typing as tp
+from nevergrad.parametrization.utils import TemporaryDirectoryCopy, CommandFunction
 from . import variables
 from . import variables as var
 from .multivariables import Instrumentation as Instrumentation
 from .multivariables import InstrumentedFunction as InstrumentedFunction
-from nevergrad.parametrization.instantiate import FolderFunction
-from nevergrad.parametrization.utils import TemporaryDirectoryCopy, CommandFunction
+
+
+class FolderFunction:
+
+    def __init__(self, *args: tp.Any, **kwargs: tp.Any) -> None:
+        raise RuntimeError("Please import as nevergrad.parametrization.FolderFunction")
+
 
 __all__ = ["Instrumentation", "var", "CommandFunction", "FolderFunction"]
