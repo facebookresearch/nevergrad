b4e1daf7e4a246b52c03a36190215bdeb08b0797
Petr Kungurtsev <pkungurtsev@enthought.com>
2020-01-30 13:17:25 +0000

    MAINT: Node.pop method moved to the class

--- nevergrad/functions/multiobjective/hypervolume.py
+++ nevergrad/functions/multiobjective/hypervolume.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import List, AnyStr, Iterator
+from typing import List, Iterator, Optional
 
 
 class VectorNode:
@@ -12,7 +12,7 @@ class VectorNode:
         self.area = np.zeros(self.dimension)
         self.volume = np.zeros(self.dimension)
 
-    def __str__(self) -> AnyStr:
+    def __str__(self):
         return str(self.coordinate)
 
     def __lt__(self, other):
@@ -23,6 +23,15 @@ class VectorNode:
         self.area[1 : dimension + 1] = [
             -self.area[i] * self.coordinate[i] for i in range(dimension)
         ]
+
+    def pop(self, index: int) -> None:
+        """ Assigns the references of the self predecessor and successor at
+        `index` index to each other, removes the links to the `self` node.
+        """
+        predecessor = self.prev[index]
+        successor = self.next[index]
+        predecessor.next[index] = successor
+        successor.prev[index] = predecessor
 
 
 class VectorLinkedList:
@@ -51,9 +60,9 @@ class VectorLinkedList:
         self.sentinel.prev[index] = node
         current_last.next[index] = node
 
-    def extend(self, nodes: List[VectorNode], index: int) -> None:
-        """ Extends the Multilist with a list of nodes at
-        `index` position"""
+    def extend(self, nodes: List[Optional[VectorNode]], index: int) -> None:
+        """ Extends the VectorLinkedList with a list of nodes
+        at `index` position"""
         for node in nodes:
             self.append(node, index)
 
@@ -70,10 +79,7 @@ class VectorLinkedList:
         """ Removes and returns 'node' from all lists at the
         positions from 0 in index (exclusively)."""
         for i in range(index):
-            predecessor = node.prev[i]
-            successor = node.next[i]
-            predecessor.next[i] = successor
-            successor.prev[i] = predecessor
+            node.pop(i)
 
         return node
 
@@ -88,7 +94,7 @@ class VectorLinkedList:
             node.prev[i].next[i] = node
             node.next[i].prev[i] = node
 
-    def iterate(self, index: int, start: VectorNode = None) -> Iterator[VectorNode]:
+    def iterate(self, index: int, start: VectorNode = None) -> Iterator[Optional[VectorNode]]:
         if start is None:
             node = self.sentinel.next[index]
         else:
@@ -97,7 +103,7 @@ class VectorLinkedList:
             yield node
             node = node.next[index]
 
-    def reverse_iterate(self, index: int, start: VectorNode = None) -> Iterator[VectorNode]:
+    def reverse_iterate(self, index: int, start: VectorNode = None) -> Iterator[Optional[VectorNode]]:
         if start is None:
             node = self.sentinel.prev[index]
         else:
@@ -108,13 +114,13 @@ class VectorLinkedList:
 
 
 class HypervolumeIndicator:
-    def __init__(self, reference_point: List[float]) -> None:
+    def __init__(self, reference_point) -> None:
         self.reference_point = reference_point
         self.dimension = len(self.reference_point)
         self.reference_bounds = np.full(self.dimension, -np.inf)
         self.multilist = None
 
-    def compute(self, points: List[List[float]]) -> float:
+    def compute(self, points) -> float:
         points = points - self.reference_point
 
         self.multilist = self.construct_linkedlist(points)
@@ -132,7 +138,7 @@ class HypervolumeIndicator:
 
         return linkedlist
 
-    def sort_by_index(self, node_list: List[VectorNode], dimension_index: int) -> List[VectorNode]:
+    def sort_by_index(self, node_list: List[Optional[VectorNode]], dimension_index: int) -> List[Optional[VectorNode]]:
         """ Returns a sorted list of `VectorNode`, with the sorting key defined by the
         `dimension_index`-th coordinates of the nodes in the `node_list`."""
         return sorted(node_list, key=lambda node: node.coordinate[dimension_index])
@@ -236,14 +242,14 @@ class HypervolumeIndicator:
 
 
 if __name__ == "__main__":
-    reference = np.array([79, 89])
+    reference = np.array([79, 89, 99])
     hv = HypervolumeIndicator(reference)
     front = [
-        (110, 110),  # -0 + distance
-        (110, 90),  # -0 + distance
-        (80, 80),  # -400 + distance
-        (50, 50),
-        (105, 30),
+        (110, 110, 100),  # -0 + distance
+        (110, 90, 87),  # -0 + distance
+        (80, 80, 36),  # -400 + distance
+        (50, 50, 55),
+        (105, 30, 43),
     ]
     volume = hv.compute(front)
 
