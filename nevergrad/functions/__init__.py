e3f3aabbf243a5f4cf32212d70c5712dfb6ef6a3
Jeremy Rapin <jrapin@fb.com>
2020-02-07 17:12:07 +0100

    Empty

--- nevergrad/functions/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-from .functionlib import ArtificialFunction as ArtificialFunction
-from .functionlib import FarOptimumFunction as FarOptimumFunction
-from .multiobjective import MultiobjectiveFunction as MultiobjectiveFunction
-from .base import ExperimentFunction as ExperimentFunction
--- nevergrad/functions/mlda/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-from .problems import Clustering as Clustering
-from .problems import Perceptron as Perceptron
-from .problems import SammonMapping as SammonMapping
-from .problems import Landscape as Landscape
