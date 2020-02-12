2e605b7c93f3045b08a38e0fcbb4f7a6e59a158d
Jeremy Rapin <jrapin@fb.com>
2020-02-12 17:08:23 +0100

    nit

--- nevergrad/functions/games/test_game.py
+++ nevergrad/functions/games/test_game.py
@@ -13,12 +13,13 @@ from . import game
 def test_games(name: str) -> None:
     dimension = game._Game().play_game(name)
     res: tp.List[tp.Any] = []
-    for _ in range(200):
-        res += [game._Game().play_game(name, np.random.uniform(0, 1, dimension), None)]
+    for k in range(200):
+        res.append(game._Game().play_game(name, np.random.uniform(0, 1, dimension), None))
         score = (float(sum(1 if r == 2 else 0 if r == 1 else 0.5 for r in res)) / len(res))
-        if score >= 0.2 and score <= 0.8 and len(res) >= 20:
+        if k >= 20 and 0.2 <= score <= 0.8:
             break
     assert score >= 0.1
     assert score <= 0.9
+    assert any(res), "All ties"
     function = game.Game(name)
     function(function.parametrization.random_state.normal(size=function.dimension))
