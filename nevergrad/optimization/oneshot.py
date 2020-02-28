bc458a5a434f95d22916f43672f12602a717fd1a
Teytaud <oteytaud@fb.com>
2020-02-28 15:57:18 +0100

    Update oneshot.py

--- nevergrad/optimization/oneshot.py
+++ nevergrad/optimization/oneshot.py
@@ -40,7 +40,7 @@ def avg_of_k_best(archive: utils.Archive[utils.Value], method: str = "dimfourth"
         k = max(1, len(archive) // (2**dimension))
     elif method == "hull":
         k = np.concatenate(
-            convex_limit(sorted(items[0], key=lambda indiv: archive[indiv[0]].get_estimation("pessimistic"))), axis=0)
+            convex_limit(sorted(items, key=lambda indiv: archive[indiv[0]].get_estimation("pessimistic"))), axis=0)
     else:
         raise ValueError(f"{method} not implemented as a method for choosing k in avg_of_k_best.")
     k = 1 if k < 1 else k
