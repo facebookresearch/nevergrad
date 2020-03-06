66e21f1d91a955545117da7f89372c0b9b5e0e54
Teytaud <oteytaud@fb.com>
2020-03-06 11:14:20 +0100

    option for meunier style (#542)
    * option for meunier style
    
    * Update oneshot.py
    
    * Update oneshot.py
    
    * Update oneshot.py
    
    * fix
    
    * Update oneshot.py
    
    * convexhull
    
    * fix
    
    * fix
    
    * Update oneshot.py
    
    * Update experimentalvariants.py
    
    * Update oneshot.py
    
    * Update oneshot.py
    
    * Update oneshot.py
    
    * Update oneshot.py
    
    * Update oneshot.py
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Update oneshot.py (#572)
    
    * Update oneshot.py
    
    * Update recorded_recommendations.csv
    
    * Update experiments.py
    
    * Update optimizerlib.py
    
    * Update plotting.py
    
    * Update plotting.py
    
    * Update plotting.py
    
    * Update plotting.py

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -797,7 +797,8 @@ def photonics(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
     es += [ng.families.EvolutionStrategy(recombination_ratio=recomb, only_offsprings=only, popsize=pop,
                                          offsprings={40: 60, 100: 150}[pop])
            for only in [True, False] for recomb in [0.1, 1] for pop in popsizes]
-    algos = ["TwoPointsDE", "DE", "RealSpacePSO", "PSO", "OnePlusOne", "ParametrizationDE", "NaiveTBPSA"] + es  # type: ignore
+    algos = ["TwoPointsDE", "DE", "RealSpacePSO", "PSO", "OnePlusOne", "ParametrizationDE", "NaiveTBPSA",
+        "SplitOptimizer5", "Shiva", "NGO", "MultiCMA", "CMandAS2", "SplitOptimizer13"] + es  # type: ignore
     for method in ["clipping", "tanh", "arctan"]:
         # , "chirped"]]:  # , "morpho"]]:
         for func in [Photonics(x, 60 if x == "morpho" else 80, bounding_method=method) for x in ["bragg"]]:
