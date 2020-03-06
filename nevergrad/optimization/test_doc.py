9c6656d6af1f2df20790cd20b346fdef75d8e52c
Olivier Teytaud <oteytaud@fb.com>
2020-03-06 17:58:03 +0200

    fix

--- nevergrad/optimization/test_doc.py
+++ nevergrad/optimization/test_doc.py
@@ -1,3 +1,7 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 import warnings
 import numpy as np
 import nevergrad as ng
