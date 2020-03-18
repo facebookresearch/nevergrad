62f63362a528c823cf5a3b408b0a554fabeefe5f
Jérémy Rapin <jrapin.github@gmail.com>
2020-03-18 10:17:20 +0100

    Update mutation.py

--- nevergrad/parametrization/mutation.py
+++ nevergrad/parametrization/mutation.py
@@ -2,6 +2,8 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+"""Experimental mutation patterns for structured data
+"""
 
 import typing as tp
 import numpy as np
