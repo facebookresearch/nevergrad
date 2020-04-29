52e2e68020dd6123ee634d73f2caa5a6f107b1c2
Teytaud <oteytaud@fb.com>
2020-04-29 17:43:06 +0200

    Add arity in descriptors (#633)
    * arity in descriptors
    
    * Update experiments.py
    
    * Update base.py
    
    * Update base.py
    
    * Update nevergrad/functions/base.py
    
    Thx for having done it yourself :-)
    
    Co-Authored-By: Jérémy Rapin <jrapin.github@gmail.com>
    
    * Update nevergrad/benchmark/experiments.py
    
    Co-Authored-By: Jérémy Rapin <jrapin.github@gmail.com>
    
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -108,6 +108,7 @@ def wide_discrete(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
             instrum = ng.p.Tuple(*(ng.p.TransitionChoice(range(arity)) for _ in range(nv)))
             for discrete_func in [corefuncs.onemax, corefuncs.leadingones, corefuncs.jump]:
                 dfunc = ExperimentFunction(discrete_func, instrum)
+                dfunc.add_descriptors(arity=arity)
                 for optim in optims:
                     for nw in [1, 10]:
                         for budget in [500, 5000]:
