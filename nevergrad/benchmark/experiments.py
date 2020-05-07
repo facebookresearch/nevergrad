917eb46333a21287716121e0b91da4acb14082d9
Olivier Teytaud <oteytaud@fb.com>
2020-05-07 21:09:59 +0200

    testing_more.

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -44,7 +44,8 @@ def mltuning(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
               "DoubleFastGADiscreteOnePlusOne"]
     for dimension in [None, 1, 2, 3]:
         for regressor in ["mlp", "decision_tree", "decision_tree_depth"]:
-            for dataset in ["boston", "diabetes"] if dimension is None else ["artificialcos", "artificial", "artificialsquare"]:
+            for dataset in (["boston", "diabetes"] if dimension is None else ["artificialcos", "artificial", "artificialsquare"]):
+                assert dataset[:10] != "artificial" or dimension is not None
                 function = MLTuning(regressor, dimension, dataset)
                 for budget in [50, 150, 500]:
                     for num_workers in [1, 10, 50, 100]:
