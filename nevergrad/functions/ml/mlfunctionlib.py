9124d5456b884d51a5a77642d21b6b9a3efdfb2c
Teytaud <oteytaud@fb.com>
2020-05-07 10:43:11 +0200

    Adding documentation.

--- nevergrad/functions/ml/mlfunctionlib.py
+++ nevergrad/functions/ml/mlfunctionlib.py
@@ -76,21 +76,26 @@ class MLTuning(ExperimentFunction):
             # Predict
             pred_test = regr.predict(X_test)
             result += np.sum((y_test - pred_test)**2)
-        return result / num_data
+        return result / num_data  # We return a 10-fold validation error.
 
     def __init__(self, regressor: str, dimension: int):
+        """We propose different possible regressors and different dimensionalities.
+        In each case, Nevergrad will optimize the parameters of a scikit learning.
+        """
         self.regressor = regressor
         self.name = regressor + f"Dim{dimension}"
 
         if regressor == "decision_tree_depth":
-            # Only the depth
-            parametrization = p.Instrumentation(depth=p.Scalar(lower=1, upper=1200).set_integer_casting())        
+            # Only the depth, as an evaluation.
+            parametrization = p.Instrumentation(depth=p.Scalar(lower=1, upper=1200).set_integer_casting())  
+            # We optimize only the depth, so we fix all other parameters than the depth, using "partial".
             super().__init__(partial(self._ml_parametrization,
                                      noise_free=False, criterion="mse",
                                      min_samples_split=0.00001, dimension=dimension,
                                      regressor="decision_tree",
                                      alpha=1.0, learning_rate="no", 
                                      activation="no", solver="no"), parametrization)
+            # For the evaluation, we remove the noise.
             self.evaluation_function = partial(self._ml_parametrization,  # type: ignore
                                                noise_free=True, criterion="mse", 
                                                dimension=dimension, min_samples_split=0.00001,
@@ -98,35 +103,42 @@ class MLTuning(ExperimentFunction):
                                                alpha=1.0, learning_rate="no", 
                                                activation="no", solver="no")
         elif regressor == "any":
+            # First we define the list of parameters in the optimization
             parametrization = p.Instrumentation(
-                depth=p.Scalar(lower=1, upper=1200).set_integer_casting(),
-                criterion=p.Choice(["mse", "friedman_mse", "mae"]),
-                min_samples_split=p.Log(lower=0.0000001, upper=1),
-                regressor=p.Choice(["mlp", "decision_tree"]),
-                activation=p.Choice(["identity", "logistic", "tanh", "relu"]),
-                solver=p.Choice(["lbfgs", "sgd", "adam"]),
-                learning_rate=p.Choice(["constant", "invscaling", "adaptive"]),
-                alpha=p.Log(lower=0.0000001, upper=1.),
+                depth=p.Scalar(lower=1, upper=1200).set_integer_casting(),  # Depth, in case we use a decision tree.
+                criterion=p.Choice(["mse", "friedman_mse", "mae"]),  # Criterion for building the decision tree.
+                min_samples_split=p.Log(lower=0.0000001, upper=1),  # Min ratio of samples in a node for splitting.
+                regressor=p.Choice(["mlp", "decision_tree"]),  # Type of regressor.
+                activation=p.Choice(["identity", "logistic", "tanh", "relu"]),  # Activation function, in case we use a net.
+                solver=p.Choice(["lbfgs", "sgd", "adam"]),  # Numerical optimizer.
+                learning_rate=p.Choice(["constant", "invscaling", "adaptive"]),  # Learning rate schedule.
+                alpha=p.Log(lower=0.0000001, upper=1.),  # Complexity penalization.
             )        
+            # Only the dimension is fixed, so "partial" is just used for fixing the dimension.
+            # noise_free is False (meaning that we consider the cross-validation loss) during the optimization.
             super().__init__(partial(self._ml_parametrization, dimension=dimension, noise_free=False), parametrization)
+            # For the evaluation we use the test set, which is big, so noise_free = True.
             self.evaluation_function = partial(self._ml_parametrization, dimension=dimension, noise_free=True)  # type: ignore
         elif regressor == "decision_tree":
-            # Adding criterion{“mse”, “friedman_mse”, “mae”}
+            # We specify below the list of hyperparameters for the decision trees.
             parametrization = p.Instrumentation(
                 depth=p.Scalar(lower=1, upper=1200).set_integer_casting(),
                 criterion=p.Choice(["mse", "friedman_mse", "mae"]),
                 min_samples_split=p.Log(lower=0.0000001, upper=1),
                 regressor="decision_tree",
             )        
+            # We use "partial" for fixing the parameters of the neural network, given that we work on the decision tree only.
             super().__init__(partial(self._ml_parametrization, dimension=dimension, noise_free=False,        
                                      alpha=1.0, learning_rate="no", regressor="decision_tree", 
                                      activation="no", solver="no"), parametrization)
+            # For the test we just switch noise_free to True.
             self.evaluation_function = partial(self._ml_parametrization, dimension=dimension, criterion="mse",  # type: ignore
                                                min_samples_split=0.00001,
                                                regressor="decision_tree", noise_free=True,        
                                                alpha=1.0, learning_rate="no", 
                                                activation="no", solver="no")
         elif regressor == "mlp":
+            # Let us define the parameters of the neural network.
             parametrization = p.Instrumentation(
                 activation=p.Choice(["identity", "logistic", "tanh", "relu"]),
                 solver=p.Choice(["lbfgs", "sgd", "adam"]),
@@ -134,6 +146,8 @@ class MLTuning(ExperimentFunction):
                 learning_rate=p.Choice(["constant", "invscaling", "adaptive"]),
                 alpha=p.Log(lower=0.0000001, upper=1.),
             )        
+            # And, using partial, we get rid of the parameters of the decision tree (we work on the neural net, not
+            # on the decision tree).
             super().__init__(partial(self._ml_parametrization, dimension=dimension, noise_free=False,
                                      regressor="mlp", depth=-3, criterion="no", min_samples_split=0.1), parametrization)
             self.evaluation_function = partial(self._ml_parametrization, dimension=dimension,  # type: ignore
