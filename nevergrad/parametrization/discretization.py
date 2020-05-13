f32049e0cfa81d9d0c423db805aac5a99d50f10c
Jérémy Rapin <jrapin.github@gmail.com>
2020-05-13 16:46:42 +0200

    Allow repetitions for Choice parameters (#670)

--- nevergrad/parametrization/discretization.py
+++ nevergrad/parametrization/discretization.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import List, Union, Any
+import typing as tp
 import warnings
 import numpy as np
 import scipy.stats
@@ -15,7 +15,7 @@ from ..common.typetools import ArrayLike
 # - by a softmax transformation, a k-valued categorical variable is converted into k continuous variables.
 # - by a discretization - as we often use Gaussian random values, we discretize according to quantiles of the normal
 #   distribution.
-def threshold_discretization(x: ArrayLike, arity: int = 2) -> List[int]:
+def threshold_discretization(x: ArrayLike, arity: int = 2) -> tp.List[int]:
     """Discretize by casting values from 0 to arity -1, assuming that x values
     follow a normal distribution.
 
@@ -41,7 +41,7 @@ def threshold_discretization(x: ArrayLike, arity: int = 2) -> List[int]:
 
 
 # The function below is the opposite of the function above.
-def inverse_threshold_discretization(indexes: List[int], arity: int = 2) -> np.ndarray:
+def inverse_threshold_discretization(indexes: tp.List[int], arity: int = 2) -> np.ndarray:
     indexes_arr = np.array(indexes, copy=True)
     pdf_bin_size = 1 / arity
     # We take the center of each bin (in the pdf space)
@@ -50,64 +50,80 @@ def inverse_threshold_discretization(indexes: List[int], arity: int = 2) -> np.n
 
 # The discretization is, by nature, not one to one.
 # In the function below, we randomly draw one of the possible inverse values - this is therefore noisy.
-def noisy_inverse_threshold_discretization(indexes: List[int], arity: int = 2, gen: Any = None) -> np.ndarray:
+def noisy_inverse_threshold_discretization(indexes: tp.List[int], arity: int = 2, gen: tp.Any = None) -> np.ndarray:
     indexes_arr = np.array(indexes, copy=True)
     pdf_bin_size = 1 / arity
     # We take a random point in the bin.
     return scipy.stats.norm.ppf(indexes_arr * pdf_bin_size + gen.rand() * pdf_bin_size)  # type: ignore
 
 
-def softmax_discretization(x: ArrayLike, arity: int = 2, random: Union[bool, np.random.RandomState] = True) -> List[int]:
-    """Discretize a list of floats to a list of ints based on softmax probabilities.
-    For arity n, a softmax is applied to the first n values, and the result
-    serves as probability for the first output integer. The same process it
-    applied to the other input values.
+def weight_for_reset(arity: int) -> float:
+    """p is an arbitrary probability that the provided arg will be sampled with the returned point
+    """
+    p = (1 / arity) * 1.5
+    w = float(np.log((p * (arity - 1)) / (1 - p)))
+    return w
+
+
+class Encoder:
+    """Handles softmax weights which need to be turned into probabilities and sampled
+    This class is expected to evolve to be more usable and include more features (like
+    conversion from probabilities to weights?)
+    It will replace most of the code above if possible
 
     Parameters
     ----------
-    x: list/array
-        the float values from a continuous space which need to be discretized
-    arity: int
-        the number of possible integer values (arity 2 will lead to values in {0, 1})
-    random: bool or np.random.RandomState
-        either a RandomState to pull values from, or True for pulling values on the default random state,
-        or False to get a deterministic behavior
+    weights: array
+        the weights of size samples x options, that will be turned to probabilities
+        using softmax.
+    rng: RandomState
+        random number generator for sampling following the probabilities
 
     Notes
     -----
-    - if one or several inf values are present, only those are considered
+    - if one or several inf values are present in a row, only those are considered
     - in case of tie, the deterministic value is the first one (lowest) of the tie
     - nans and -infs are ignored, except if all are (then uniform random choice)
     """
-    data = np.array(x, copy=True, dtype=float).reshape((-1, arity))
-    if np.any(np.isnan(data)):
-        warnings.warn("Encountered NaN values for discretization")
-        data[np.isnan(data)] = -np.inf
-    if random is False:
-        output = np.argmax(data, axis=1).tolist()
-        return output  # type: ignore
-    if isinstance(random, bool):  # equivalent to "random is True"
-        random = np.random  # default random number generator (creating a RandomState is slow)
-    return [random.choice(arity, p=softmax_probas(d)) for d in data]
 
+    def __init__(self, weights: np.ndarray, rng: np.random.RandomState) -> None:
+        self.weights = np.array(weights, copy=True, dtype=float)
+        self.weights[np.isnan(self.weights)] = -np.inf  # 0 proba for nan values
+        self._rng = rng
 
-def softmax_probas(data: np.ndarray) -> np.ndarray:
-    # TODO: test directly? (currently through softmax discretization)
-    # TODO: move nan case here?
-    maxv = np.max(data)
-    if np.abs(maxv) == np.inf or np.isnan(maxv):
-        maxv = 0
-    data = np.exp(data - maxv)
-    if any(x == np.inf for x in data):  # deal with infinite positives special case
-        data = np.array([int(x == np.inf) for x in data])
-    if not sum(data):
-        data = np.ones(len(data))
-    return data / np.sum(data)  # type: ignore
+    def probabilities(self) -> np.ndarray:
+        """Creates the probability matrix from the weights
+        """
+        axis = 1
+        maxv = np.max(self.weights, axis=1, keepdims=True)
+        hasposinf = np.isposinf(maxv)
+        maxv[np.isinf(maxv)] = 0  # avoid indeterminations
+        exp: np.ndarray = np.exp(self.weights - maxv)
+        # deal with infinite positives special case
+        # by ignoring (0 proba) non-infinte on same row
+        if np.any(hasposinf):
+            is_inf = np.isposinf(self.weights)
+            is_ignored = np.logical_and(np.logical_not(is_inf), hasposinf)
+            exp[is_inf] = 1
+            exp[is_ignored] = 0
+        # random choice if sums to 0
+        sums0 = np.sum(exp, axis=axis) == 0
+        exp[sums0, :] = 1
+        exp /= np.sum(exp, axis=axis, keepdims=True)  # normalize
+        return exp
 
+    def encode(self, deterministic: bool = False) -> np.ndarray:
+        """Sample an index from each row depending on the provided probabilities.
 
-def inverse_softmax_discretization(index: int, arity: int) -> np.ndarray:
-    # p is an arbitrary probability that the provided arg will be sampled with the returned point
-    p = (1 / arity) * 1.5
-    x: np.ndarray = np.zeros(arity)
-    x[index] = np.log((p * (arity - 1)) / (1 - p))
-    return x
+        Parameters
+        ----------
+        deterministic: bool
+            set to True for sampling deterministically the more likely option
+            (largest probability)
+        """
+        axis = 1
+        if deterministic:
+            return np.argmax(self.weights, axis=1)  # type: ignore
+        cumprob = np.cumsum(self.probabilities(), axis=axis)
+        rand = self._rng.rand(cumprob.shape[0], 1)
+        return np.argmin(cumprob < rand, axis=axis)  # type: ignore
