4cbb4ac059207019e4887c9914cbf44d1e9fc678
Teytaud <oteytaud@fb.com>
2020-05-26 16:32:19 +0200

    Update functionlib.py

--- nevergrad/functions/functionlib.py
+++ nevergrad/functions/functionlib.py
@@ -48,11 +48,9 @@ class ArtificialVariable:
         if not self._transforms:
             self._initialize()
         if self.hashing:
-            state = np.random.get_state()
             for i, y in enumerate(data):
-                np.random.seed(int(int(hashlib.md5(str(y).encode()).hexdigest(), 16) % 500000))  # type: ignore
-                data[i] = np.random.normal(0., 1.)  # type: ignore
-            np.random.set_state(state)
+                rs = np.random.RandomState(int(int(hashlib.md5(str(y).encode()).hexdigest(), 16) % 500000))  # type: ignore
+                data[i] = rs.normal(0., 1.)  # type: ignore
         data = np.array(data, copy=False)
         output = []
         for transform in self._transforms:
