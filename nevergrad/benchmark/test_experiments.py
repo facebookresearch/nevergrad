d75c7117132fa64c8d4add687d0f044270e76eab
Teytaud <oteytaud@fb.com>
2020-05-27 18:24:47 +0200

    Update test_experiments.py

--- nevergrad/benchmark/test_experiments.py
+++ nevergrad/benchmark/test_experiments.py
@@ -20,7 +20,7 @@ def test_experiments_registry(name: str, maker: tp.Callable[[], tp.Iterator[expe
     with datasets.mocked_data():  # mock mlda data that should be downloaded
         check_maker(maker)  # this is to extract the function for reuse if other external packages need it
     if name not in {"realworld_oneshot", "mlda", "mldaas", "realworld", "rocket",
-                    "naivemltuning", "seqmltuning", "naiveseqmltuning"}:
+                    "naivemltuning", "seqmltuning", "naiveseqmltuning", "mltuning"}:
         check_seedable(maker, "mltuning" in name)  # this is a basic test on first elements, do not fully rely on it
 
 
