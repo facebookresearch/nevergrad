4f6261ed59c74f69b208d00baf36264f64c3b878
Teytaud <oteytaud@fb.com>
2020-06-23 15:13:22 +0200

    Adaptive One Plus One (#736)
    * Adaptive One Plus One
    
    * Update experiments.py
    
    * Update optimizerlib.py
    
    * Update optimizerlib.py
    
    * Update optimizerlib.py
    
    * record

--- nevergrad/optimization/optimizerlib.py
+++ nevergrad/optimization/optimizerlib.py
@@ -69,8 +69,10 @@ class _OnePlusOne(base.Optimizer):
                 assert isinstance(noise_handling, tuple), "noise_handling must be a string or  a tuple of type (strategy, factor)"
                 assert noise_handling[1] > 0.0, "the factor must be a float greater than 0"
                 assert noise_handling[0] in ["random", "optimistic"], f"Unkwnown noise handling: '{noise_handling}'"
-        assert mutation in ["gaussian", "cauchy", "discrete", "fastga", "doublefastga",
+        assert mutation in ["gaussian", "cauchy", "discrete", "fastga", "doublefastga", "adaptive",
                             "portfolio", "discreteBSO"], f"Unkwnown mutation: '{mutation}'"
+        if mutation == "adaptive":
+            self._adaptive_mr = 0.5
         self.noise_handling = noise_handling
         self.mutation = mutation
         self.crossover = crossover
@@ -115,6 +117,8 @@ class _OnePlusOne(base.Optimizer):
                     data = mutator.portfolio_discrete_mutation(pessimistic_data)
                 else:
                     data = mutator.crossover(pessimistic_data, mutator.get_roulette(self.archive, num=2))
+            elif mutation == "adaptive":
+                data = mutator.portfolio_discrete_mutation(pessimistic_data, max(1, int(self._adaptive_mr * self.dimension)))
             elif mutation == "discreteBSO":
                 assert self.budget is not None, "DiscreteBSO needs a budget."
                 intensity: int = int(self.dimension - self._num_ask * self.dimension / self.budget)
@@ -134,6 +138,9 @@ class _OnePlusOne(base.Optimizer):
     def _internal_tell(self, x: ArrayLike, value: float) -> None:
         # only used for cauchy and gaussian
         self._sigma *= 2.0 if value <= self.current_bests["pessimistic"].mean else 0.84
+        if self.mutation == "adaptive":
+            factor = 1.2 if value <= self.current_bests["pessimistic"].mean else 0.731  # 0.731 = 1.2**(-np.exp(1)-1)
+            self._adaptive_mr = min(1., factor * self._adaptive_mr)
 
 
 class ParametrizedOnePlusOne(base.ConfiguredOptimizer):
@@ -191,6 +198,7 @@ class ParametrizedOnePlusOne(base.ConfiguredOptimizer):
 OnePlusOne = ParametrizedOnePlusOne().set_name("OnePlusOne", register=True)
 NoisyOnePlusOne = ParametrizedOnePlusOne(noise_handling="random").set_name("NoisyOnePlusOne", register=True)
 DiscreteOnePlusOne = ParametrizedOnePlusOne(mutation="discrete").set_name("DiscreteOnePlusOne", register=True)
+AdaptiveDiscreteOnePlusOne = ParametrizedOnePlusOne(mutation="adaptive").set_name("AdaptiveDiscreteOnePlusOne", register=True)
 DiscreteBSOOnePlusOne = ParametrizedOnePlusOne(mutation="discreteBSO").set_name("DiscreteBSOOnePlusOne", register=True)
 CauchyOnePlusOne = ParametrizedOnePlusOne(mutation="cauchy").set_name("CauchyOnePlusOne", register=True)
 OptimisticNoisyOnePlusOne = ParametrizedOnePlusOne(
