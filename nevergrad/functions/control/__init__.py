e937ebf84b5f1dcf70a7eefa51fcb691f6a984af
Herilalaina Rakotoarison <rkt.herilalaina@gmail.com>
2020-08-15 09:16:40 +0200

    Update nevergrad/functions/control/__init__.py
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>

--- nevergrad/functions/control/__init__.py
+++ nevergrad/functions/control/__init__.py
@@ -3,9 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .core import Ant
-from .core import Swimmer
-from .core import HalfCheetah
-from .core import Hopper
-from .core import Walker2d
-from .core import Humanoid
+from .core import Ant as Ant
+from .core import Swimmer as Swimmer
+from .core import HalfCheetah as HalfCheetah
+from .core import Hopper as Hopper
+from .core import Walker2d as Walker2d
+from .core import Humanoid as Humanoid
