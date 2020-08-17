2c01246eb56fd550eebec2727546ecc3d753e1c4
Herilalaina Rakotoarison <rkt.herilalaina@gmail.com>
2020-08-17 12:14:59 +0200

    Update nevergrad/functions/control/core.py
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>

--- nevergrad/functions/control/core.py
+++ nevergrad/functions/control/core.py
@@ -16,6 +16,7 @@ class BaseFunction(ExperimentFunction):
         self.num_rollouts = num_rollouts
         self.random_state = random_state
         self.register_initialization(num_rollouts=num_rollouts, random_state=random_state)
+        self.update_descriptors(num_rollouts=num_rollouts)
 
     def _simulate(self, x: np.ndarray) -> float:
         env = GenericMujocoEnv(env_name=self.env_name,
