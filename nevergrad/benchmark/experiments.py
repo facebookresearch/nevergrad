82da42f1c256f05b283d4e1b075156527ef150e4
Laurent Meunier <laurentmeunier@fb.com>
2020-08-28 11:06:21 +0200

    fix typo

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -1051,7 +1051,7 @@ def mldakmeans(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
                             yield xp
 
 
-@registry.registerx
+@registry.register
 def arcoating(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
     """AR coating. Problems about optical properties of nanolayers."""
     seedg = create_seed_generator(seed)
@@ -1345,7 +1345,7 @@ def adversarial_attack(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]
     seedg = create_seed_generator(seed)
     optims = ["CMA", "Shiwa", "DE", "PSO", "RecES", "RecMixES", "RecMutDE", "ParametrizationDE"]
     for i, (data, target) in enumerate(data_loader):
-        if i > 1000:
+        if i > 1:
             continue
         _, pred = torch.max(classifier(data), axis=1)
         if pred == target:
