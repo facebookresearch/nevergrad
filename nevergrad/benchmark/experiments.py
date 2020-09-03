700456fb3301431d918bab60218b8b7ab5403ff1
Teytaud <oteytaud@fb.com>
2020-09-03 09:28:48 +0200

    Update experiments.py

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -900,7 +900,7 @@ def realworld(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
                             yield xp
 
 
-@registry.register
+#@registry.register  # We do not register for now -- gray-box constraints (constraints with float values) first.
 def pyomo(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
     """Pyomo models.
     TODO: we need a comparison with Pyomo solves, and non-linear/complex variants of this.
