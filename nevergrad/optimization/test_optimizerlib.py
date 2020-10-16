bd391c183877e70536eb1d6ede014d8f4232ed4d
Jeremy Rapin <jrapin@fb.com>
2020-10-16 15:58:55 +0200

    remove

--- nevergrad/optimization/test_optimizerlib.py
+++ nevergrad/optimization/test_optimizerlib.py
@@ -178,8 +178,6 @@ def test_optimizers_suggest(name: str) -> None:  # pylint: disable=redefined-out
 # pylint: disable=redefined-outer-name
 @pytest.mark.parametrize("name", registry)  # type: ignore
 def test_optimizers_recommendation(name: str, recomkeeper: RecommendationKeeper) -> None:
-    # if "BO" in name:
-    #     raise SkipTest("BO not cool these days for some reason!")
     # set up environment
     optimizer_cls = registry[name]
     if name in UNSEEDABLE:
@@ -454,6 +452,7 @@ def test_shiwa_dim1() -> None:
 )
 def test_shiwa_selection(name: str, param: tp.Any, budget: int, num_workers: int, expected: str, caplog: tp.Any) -> None:
     with caplog.at_level(logging.DEBUG, logger="nevergrad.optimization.optimizerlib"):
+        # pylint: disable=expression-not-assigned
         optlib.registry[name](param, budget=budget, num_workers=num_workers).optim  # type: ignore
         pattern = rf".*{name} selected (?P<name>\w+?) optimizer\."
         match = re.match(pattern, caplog.text.splitlines()[-1])
