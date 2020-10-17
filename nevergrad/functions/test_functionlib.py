35c48e09dd1eac0038ae42e7c74a98d2ca9c3e21
Olivier Teytaud <oteytaud@fb.com>
2020-10-17 12:38:57 +0200

    remove_deprecated_test

--- nevergrad/functions/test_functionlib.py
+++ nevergrad/functions/test_functionlib.py
@@ -50,7 +50,6 @@ def test_ptb_no_overfitting() -> None:
     sphere=({"name": "sphere", "block_dimension": 3, "useless_variables": 6, "num_blocks": 2}, 9.630),
     cigar=({"name": "cigar", "block_dimension": 3, "useless_variables": 6, "num_blocks": 2}, 3527289.665),
     cigar_rot=({"rotation": True, "name": "cigar", "block_dimension": 3, "useless_variables": 6, "num_blocks": 2}, 5239413.576),
-    no_transform=({"name": "leadingones5", "block_dimension": 50, "useless_variables": 10}, 10.0),
     hashed=({"name": "sphere", "block_dimension": 3, "useless_variables": 6, "num_blocks": 2, "hashing": True}, 12.44),
     noisy_sphere=({"name": "sphere", "block_dimension": 3, "useless_variables": 6, "num_blocks": 2, "noise_level": .2}, 9.576),
     noisy_very_sphere=({"name": "sphere", "block_dimension": 3, "useless_variables": 6,
