b5d959576ab7283930c5fbfd7aa73076ebc258fd
Olivier Teytaud <oteytaud@fb.com>
2020-10-27 12:19:19 +0100

    convert_to_str_for_correct_typing

--- nevergrad/benchmark/plotting.py
+++ nevergrad/benchmark/plotting.py
@@ -34,7 +34,7 @@ def _slugify(value: str) -> str:
     Normalizes string, converts to lowercase, removes non-alpha characters,
     and converts spaces to hyphens.
     """
-    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = str(unicodedata.normalize('NFKD', value).encode('ascii', 'ignore'))
     value = re.sub('[^\w\s-]', '', value).strip().lower()
     value = re.sub('[-\s]+', '-', value)
     # ...
