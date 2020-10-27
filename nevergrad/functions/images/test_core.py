81061986dd9f5d118dce49bc010a244acbf5ba4d
Jérémy Rapin <jrapin.github@gmail.com>
2020-10-27 16:36:32 +0100

    Restructure adversarial benchmark (#833)

--- nevergrad/functions/images/test_core.py
+++ nevergrad/functions/images/test_core.py
@@ -8,17 +8,22 @@ from . import core
 
 
 def test_images_adversarial() -> None:
-    epsilon = 0.05
-    targeted = False
-    label = 3
-    func = core.ImageAdversarial.from_testbed("test", label=label, targeted=targeted, epsilon=epsilon)
+    func = next(core.ImageAdversarial.make_folder_functions(None, model="test"))
     x = np.zeros(func.image.shape)
-
-    value = func(x)  # should not touch boundaries, so value should be < np.inf
+    value = func(x)
     assert value < np.inf
-    other_func = func.copy()
+    other_func = func.copy().copy()
     value2 = other_func(x)
-    assert value2 < np.inf
+    assert value2 == value  # same function
+
+
+def test_image_adversarial_eval() -> None:
+    func = next(core.ImageAdversarial.make_folder_functions(None, model="test"))
+    output = func.evaluation_function(func.parametrization.value)
+    assert output == 0
+    func.targeted = True
+    output = func.evaluation_function(func.parametrization.value)
+    assert output == 1
 
 
 def test_images() -> None:
