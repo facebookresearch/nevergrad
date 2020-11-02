a13dccdb8d0092d966ee24016db2067c553b93be
Teytaud <oteytaud@fb.com>
2020-11-02 10:44:01 +0100

    Update core.py

--- nevergrad/functions/control/core.py
+++ nevergrad/functions/control/core.py
@@ -41,7 +41,7 @@ class BaseFunction(ExperimentFunction):
 
     def __init__(self, num_rollouts: int, random_state: tp.Optional[int] = None) -> None:
         if self.policy_2ndlayer_dim is not None:
-            super().__init__(self._simulate, p.Tuple(p.Array(shape=self.policy_dim), p.Array(shape=self.policy_2ndlayer_dim)))
+            super().__init__(self._simulate, np.p.Instrumentation(p.Array(shape=self.policy_dim), p.Array(shape=self.policy_2ndlayer_dim)))
         else:
             super().__init__(self._simulate, p.Array(shape=self.policy_dim))
         self.num_rollouts = num_rollouts
