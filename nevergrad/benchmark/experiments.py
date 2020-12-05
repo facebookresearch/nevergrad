8bcdb415b7ab0be43927002a6d97de7a0f5c7b7f
Teytaud <olivier.teytaud@gmail.com>
2020-12-05 18:46:43 +0100

    Update experiments.py

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -66,7 +66,7 @@ def keras_tuning(seed: tp.Optional[int] = None, overfitter: bool = False, seq: b
                 function = MLTuning(regressor=regressor, data_dimension=dimension, dataset=dataset,
                                     overfitter=overfitter)
                 for budget in [50, 150, 500]:
-                    for num_workers in [1] if seq else [num_workers]:  # Seq for sequential optimization experiments.
+                    for num_workers in [1] if seq else [budget]:  # Seq for sequential optimization experiments.
                         for optim in optims:
                             xp = Experiment(function, optim, num_workers=num_workers,
                                             budget=budget, seed=next(seedg))
