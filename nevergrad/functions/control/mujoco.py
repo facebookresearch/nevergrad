6c074fdf2347ae8a4d2cf241d361afc81035e31c
herilalaina <rkt.herilalaina@gmail.com>
2020-12-09 14:21:11 +0100

    refractor neuro mujoco

--- nevergrad/functions/control/mujoco.py
+++ nevergrad/functions/control/mujoco.py
@@ -23,17 +23,27 @@ class GenericMujocoEnv:
     random_state: int or None
         random state for reproducibility in Gym environment.
     """
+
     def __init__(self, env_name, state_mean, state_std, num_rollouts,
-                 random_state):
-        self.mean = np.array(state_mean)
-        self.std = np.array(state_std)
+                 activation, layer_rescaling_coef, random_state):
+        self.mean = state_mean
+        self.std = state_std
         self.env = gym.make(env_name)
         self.num_rollouts = num_rollouts
         self.env.seed(random_state)
+        self.activation = activation
+        self.layer_rescaling_coef = layer_rescaling_coef
 
-    def __call__(self, x, y=None):
-        """Compute average cummulative reward of a given policy.
-        Optionally, y is a second (neural) layer of weights.
+    def _activation(self, x):
+        if self.activation == 'tanh':
+            return np.tanh(x)
+        elif self.activation == "sigmoid":
+            return 1. / (1 + np.exp(-x))
+        else:
+            raise NotImplementedError(r"Activation {self.activation} not implemented.")
+
+    def __call__(self, *layers):
+        """Compute loss (average cumulative negative reward) of a given policy.
         """
         returns = []
         for _ in range(self.num_rollouts):
@@ -41,15 +51,18 @@ class GenericMujocoEnv:
             done = False
             totalr = 0.
             while not done:
-                action = np.dot(x, (obs - self.mean) / self.std)
-                if y is not None:
-                    action = 0.1 * action
-                    if "Ant" in str(self.env):
-                        action = 0.1 * action
-                    if "Humanoid" in str(self.env):
-                        action = 0.01 * action
-                if y is not None:
-                    action = np.dot(np.eye(y.shape[0], y.shape[1]) + 1.e-3 + y, np.tanh(action))
+                action = np.matmul(obs, layers[0]) if (self.mean is None) else (np.matmul((obs - self.mean) / self.std, layers[0]))
+                action = action * self.layer_rescaling_coef[0]
+                for x, r_coef in zip(layers[1:], self.layer_rescaling_coef[1:]):
+                    action = np.matmul(self._activation(action) + 1.e-3, x)
+                # if y is not None:
+                #     action = 0.1 * action
+                #     if "Ant" in str(self.env):
+                #         action = 0.1 * action
+                #     if "Humanoid" in str(self.env):
+                #         action = 0.01 * action
+                # if y is not None:
+                #     action = np.dot(np.eye(y.shape[0], y.shape[1]) + 1.e-3 + y, np.tanh(action))
                 obs, r, done, _ = self.env.step(action)
                 totalr += r
             returns.append(totalr)
--- nevergrad/functions/control/test_mujoco.py
+++ nevergrad/functions/control/test_mujoco.py
@@ -3,12 +3,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import pytest
 import numpy as np
 from .core import BaseFunction
 
 class MountainCarContinuous(BaseFunction):
     env_name = "MountainCarContinuous-v0"
-    policy_dim= (1, 2)
+    policy_dim= (2, 1)
     state_mean = [0, 0]
     state_std = [1, 1]
 
@@ -16,11 +17,19 @@ def test_gym() -> None:
     func = MountainCarContinuous(num_rollouts=5)
 
     x = func.parametrization.value
-    np.testing.assert_array_equal(x, [[0., 0.]])
+    np.testing.assert_array_equal(x[0][0], [[0.], [0.]])
     value = func(x)
     np.testing.assert_almost_equal(value, 0.)
 
     x = func.parametrization.sample().value
-    assert not np.array_equal(x, [[0., 0.]])
+    assert not np.array_equal(x[0][0], [[0.], [0.]])
     value = func(func.parametrization.sample().value)
     assert value != 0
+
+def test_all_mujoco_envs() -> None:
+    core = pytest.importorskip('nevergrad.functions.control.core')
+
+    for module in ["Ant", "Swimmer", "HalfCheetah", "Hopper", "Walker2d", "Humanoid", "NeuroAnt", "NeuroSwimmer", "NeuroHalfCheetah", "NeuroHopper", "NeuroWalker2d", "NeuroHumanoid"]:
+        func = getattr(core, module)(num_rollouts=1)
+        for _ in range(10):
+            assert isinstance(func(func.parametrization.sample().value), float)
