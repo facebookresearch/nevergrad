5fbeaf2e4a0210e6f5e359adb1a0ba733b6f2cd5
Baptiste Rozière <broz@fb.com>
2020-12-10 18:27:42 +0100

    removing unused comment

--- nevergrad/functions/images/test_core.py
+++ nevergrad/functions/images/test_core.py
@@ -42,7 +42,6 @@ def test_images() -> None:
 def test_image_from_pgan_with_l1_loss() -> None:
     func = core.ImageFromPGAN(initial_noise=None, use_gpu=False, loss=imagelosses.SumAbsoluteDifferences(reference=np.zeros((1, 512, 512, 3))))
     x = 7 * np.fabs(np.random.normal(size=func.noise_shape))
-    # data = func.parametrization.spawn_child().set_standardized_data(x.flatten()).value
     value = func(x)  # should not touch boundaries, so value should be < np.inf
     assert value < np.inf
     other_func = func.copy()
