73a4afc25302acd9c4950d3f63d8bd9e49b9b2e0
Olivier Teytaud <oteytaud@fb.com>
2020-12-10 12:59:28 +0200

    remove_loop

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -60,18 +60,17 @@ def keras_tuning(seed: tp.Optional[int] = None, overfitter: bool = False, seq: b
     if default_optims is not None:
         optims = default_optims
     for dimension in [None]:
-        for regressor in ["keras_dense_nn"]:
-            for dataset in (
-                    ["kerasBoston", "boston", "diabetes"]):
-                function = MLTuning(regressor=regressor, data_dimension=dimension, dataset=dataset,
-                                    overfitter=overfitter)
-                for budget in [50, 150, 500]:
-                    for num_workers in [1] if seq else [1, 10, 50, 100]:  # Seq for sequential optimization experiments.
-                        for optim in optims:
-                            xp = Experiment(function, optim, num_workers=num_workers,
-                                            budget=budget, seed=next(seedg))
-                            if not xp.is_incoherent:
-                                yield xp
+        for dataset in (
+                ["kerasBoston", "boston", "diabetes"]):
+            function = MLTuning(regressor="keras_dense_nn", data_dimension=dimension, dataset=dataset,
+                                overfitter=overfitter)
+            for budget in [50, 150, 500]:
+                for num_workers in [1] if seq else [1, 10, 50, 100]:  # Seq for sequential optimization experiments.
+                    for optim in optims:
+                        xp = Experiment(function, optim, num_workers=num_workers,
+                                        budget=budget, seed=next(seedg))
+                        if not xp.is_incoherent:
+                            yield xp
 
 
 def mltuning(seed: tp.Optional[int] = None, overfitter: bool = False, seq: bool = False) -> tp.Iterator[Experiment]:
