73a3613586b4da3be788f3f31f95337fcf5a94cb
Olivier Teytaud <oteytaud@fb.com>
2020-12-14 13:03:28 +0100

    better_test

--- nevergrad/functions/images/test_imagelosses.py
+++ nevergrad/functions/images/test_imagelosses.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from pathlib import Path
+import PIL.Image
 import numpy as np
 from nevergrad.functions.images import imagelosses
 
@@ -12,6 +14,18 @@ def test_l1_loss() -> None:
     assert loss(np.ones((3, 2))) == 6
 
 
-def all_losses() -> None:
-    for loss in imageslosses.registry:[
+def test_all_losses() -> None:
+    for loss in imagelosses.registry:
         assert loss(reference=np.zeros((3, 2)))(np.ones(3, 2)) > 0.0
+
+
+def test_consistency_losses_with_oteytaud() -> None:
+    path = Path(__file__).with_name("headrgb_olivier.png")
+    image = PIL.Image.open(path).resize((64, 64), PIL.Image.ANTIALIAS)
+    data = np.asarray(image)[:, :, :3]  # 4th Channel is pointless here, only 255.
+    for loss_name, loss_class in imagelosses.registry.items():
+        loss = loss_class(reference = data)
+        random_data = np.random.uniform(low=0., high=255., size=data.size)
+        assert loss(data) < loss(random_data), f"Loss {loss_name} fails on oteytaud's photo."
+
+
