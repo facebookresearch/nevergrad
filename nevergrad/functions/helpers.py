7c33a7b830c78a35619886cc77982974a5778dbe
Olivier Teytaud <oteytaud@fb.com>
2020-12-17 10:31:52 +0200

    fix

--- nevergrad/functions/helpers.py
+++ nevergrad/functions/helpers.py
@@ -3,7 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import pathlib
 import nevergrad.common.typing as tp
 from nevergrad.parametrization import parameter as p
 from nevergrad.optimization import multiobjective as mobj
--- nevergrad/functions/images/imagelosses.py
+++ nevergrad/functions/images/imagelosses.py
@@ -3,8 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import cv2  # type: ignore
-import imquality.brisque as brisque  # type: ignore
+import cv2
+import imquality.brisque as brisque
 import lpips  # type: ignore
 import os
 import torch
