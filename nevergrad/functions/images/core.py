53303c62c1f42299555b7786422520276fabe833
Olivier Teytaud <oteytaud@fb.com>
2020-12-18 14:15:52 +0200

    255_to_256

--- nevergrad/functions/images/core.py
+++ nevergrad/functions/images/core.py
@@ -260,4 +260,4 @@ class ImageFromPGAN(base.ExperimentFunction):
         """ Generates images tensor of shape [nb_images, x, y, 3] with pixels between 0 and 255"""
         # pylint: disable=not-callable
         noise = torch.tensor(x.astype("float32"))
-        return ((self.pgan_model.test(noise).clamp(min=-1, max=1) + 1) * 255 / 2).permute(0, 2, 3, 1).cpu().numpy()  # type: ignore
+        return ((self.pgan_model.test(noise).clamp(min=-1, max=1) + 1) * 255.99 / 2).permute(0, 2, 3, 1).cpu().numpy()  # type: ignore
