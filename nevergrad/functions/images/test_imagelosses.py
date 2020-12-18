ffa1cafb27644969a0d626f1f3b6d3bdc786be2b
Olivier Teytaud <oteytaud@fb.com>
2020-12-18 12:15:49 +0200

    fix

--- nevergrad/functions/images/test_imagelosses.py
+++ nevergrad/functions/images/test_imagelosses.py
@@ -10,16 +10,18 @@ import pytest
 from nevergrad.functions.images import imagelosses
 
 
+def test_reference() -> None:
+    assert imagelosses.SumAbsoluteDifferences.REQUIRES_REFERENCE
+    assert not imagelosses.Blur.REQUIRES_REFERENCE
+    assert not imagelosses.Koncept512.REQUIRES_REFERENCE
+    assert not imagelosses.Brisque.REQUIRES_REFERENCE
+    assert len([loss for loss in imagelosses.registry.values() if loss.REQUIRES_REFERENCE]) == 5
+    assert len([loss for loss in imagelosses.registry.values() if not loss.REQUIRES_REFERENCE]) == 3
+
+
 def test_l1_loss() -> None:
     loss = imagelosses.SumAbsoluteDifferences(reference=124.0 * np.ones((300, 400, 3)))
     assert loss(np.ones((300, 400, 3))) == 44280000.0
-
-
-def test_image_losses_with_reference() -> None:
-    assert (
-        len([l for l in imagelosses.registry.values() if issubclass(l, imagelosses.ImageLossWithReference)])
-        > 2
-    )
 
 
 @pytest.mark.parametrize("loss_name", imagelosses.registry)  # type: ignore
@@ -27,6 +29,8 @@ def test_consistency_losses_with_oteytaud(loss_name: str) -> None:
     path = Path(__file__).with_name("headrgb_olivier.png")
     image = PIL.Image.open(path).resize((256, 256), PIL.Image.ANTIALIAS)
     data = np.asarray(image)[:, :, :3]  # 4th Channel is pointless here, only 255.
+
+    data_flip = np.flip(data, 0).copy()  # Copy necessary as some nets do not support negative stride.
     loss_class = imagelosses.registry[loss_name]
     loss = loss_class(reference=data)
     random_data = np.random.uniform(low=0.0, high=255.0, size=data.shape)
@@ -41,3 +45,9 @@ def test_consistency_losses_with_oteytaud(loss_name: str) -> None:
     loss_random_data = loss(random_data)
     assert loss_random_data == loss(random_data)
     assert "Blur" in loss_name or loss_data < loss_random_data, f"Loss {loss_name} fails on oteytaud's photo."
+    if loss.REQUIRES_REFERENCE:
+        assert loss_data == 0.0
+        if "Histogram" not in loss_name:
+            assert loss_data < loss(data_flip)
+        else:
+            assert loss_data == loss(data_flip)
