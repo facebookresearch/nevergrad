8713f40eb8c83751749d1eb91907fca3a48fa93f
Paco Wong <paco.pkwong@gmail.com>
2020-12-22 23:19:24 +0800

    Remove register_initialization

--- nevergrad/functions/unitcommitment/core.py
+++ nevergrad/functions/unitcommitment/core.py
@@ -55,7 +55,6 @@ class UnitCommitmentProblem(ExperimentFunction):
             operational_states=param_operational_states,
         ).set_name("")
         super().__init__(self.unit_commitment_obj_with_penalization, instru)
-        self.register_initialization(**params)
         self._descriptors.update(num_timepoints=self.num_timepoints, num_generators=self.num_generators)
 
     def unit_commitment_obj_with_penalization(self, operational_output, operational_states):
