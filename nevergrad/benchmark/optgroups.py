986f8c82a5ce8861a6c5bab401027b2b954948c4
Olivier Teytaud <oteytaud@fb.com>
2020-12-25 18:20:29 +0100

    fixtypo

--- nevergrad/benchmark/optgroups.py
+++ nevergrad/benchmark/optgroups.py
@@ -158,7 +158,7 @@ def competence_map() -> tp.Sequence[Optim]:
 @registry.register
 def competitive() -> tp.Sequence[Optim]:
     return get_optimizers("cma", "competence_map") + [
-        "MetaNGOpt10",
+        "MetaNGOpt8",
         "NaiveTBPSA",
         "PSO",
         "DE",
