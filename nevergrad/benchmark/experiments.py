aca6afee82978a0d77ac13b9fdf535d2a4c9effd
Olivier Teytaud <oteytaud@fb.com>
2021-01-08 19:43:19 +0100

    six

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -23,6 +23,7 @@ from nevergrad.functions import images as imagesxp
 from nevergrad.functions.powersystems import PowerSystem
 from nevergrad.functions.stsp import STSP
 from nevergrad.functions.rocket import Rocket
+from nevergrad.functions.six import SixGame
 from nevergrad.functions.mixsimulator import OptimizeMix
 from nevergrad.functions.unitcommitment import UnitCommitmentProblem
 from nevergrad.functions import control
@@ -949,6 +950,24 @@ def realworld(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
     optims = get_optimizers("basics", seed=next(seedg))
     for budget in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400, 12800]:
         for num_workers in [1, 10, 100]:
+            if num_workers < budget:
+                for algo in optims:
+                    for fu in funcs:
+                        xp = Experiment(fu, algo, budget, num_workers=num_workers, seed=next(seedg))
+                        if not xp.is_incoherent:
+                            yield xp
+
+
+@registry.register
+def six(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
+    """Rocket simulator. Maximize max altitude by choosing the thrust schedule, given a total thrust.
+    Budget 25, 50, ..., 1600.
+    Sequential or 30 workers."""
+    funcs = [SixGame()]
+    seedg = create_seed_generator(seed)
+    optims = get_optimizers("basics", "noisy", seed=next(seedg))
+    for budget in [25, 50, 100, 200, 400, 800, 1600]:
+        for num_workers in [1, 30]:
             if num_workers < budget:
                 for algo in optims:
                     for fu in funcs:
---
+++ nevergrad/functions/six/core.py
@@ -0,0 +1,23 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Based on https://raw.githubusercontent.com/purdue-orbital/rocket-simulation/master/Simulation2.py
+
+
+import numpy as np
+from nevergrad.parametrization import parameter as p
+from ..base import ExperimentFunction
+from .six import dimension as dimension
+from .six import play_games as play_games
+
+
+class SixGame(ExperimentFunction):
+
+    def __init__(self, num_players: int = 5) -> None:
+        self.num_players = num_players
+        super().__init__(self._simulate_six, p.Array(shape=(dimension(num_players=num_players),)))
+
+    def _simulate_six(self, x: np.ndarray) -> float:
+        return play_games(x, self.num_players)[0]
