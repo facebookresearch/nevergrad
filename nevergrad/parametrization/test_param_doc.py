fad164648f01a7e075e09480538ded6ccc5114b9
Jeremy Rapin <jrapin@fb.com>
2021-02-24 18:47:28 +0100

    rmprints

--- nevergrad/parametrization/test_param_doc.py
+++ nevergrad/parametrization/test_param_doc.py
@@ -31,9 +31,6 @@ def test_readme_parametrization() -> None:
         architecture=ng.p.Choice(["conv", "fc"]),
     )
 
-    print("ready")
-    print(parametrization)
-    print("go")
     optimizer = ng.optimizers.OnePlusOne(parametrization=parametrization, budget=100)
     recommendation = optimizer.minimize(fake_training)
 
