7a3d2767600a995c544b036d791cdc27e2fe95a3
Jeremy Rapin <jrapin@fb.com>
2021-02-26 13:52:22 +0100

    fix

--- nevergrad/ops/test_constraints.py
+++ nevergrad/ops/test_constraints.py
@@ -24,4 +24,6 @@ def test_constraint(num: int) -> None:
     layer: constraints.Constraint = constrained._layers[-1]  # type: ignore
     assert layer.function(*([1] * num)) == 1 if num == 1 else [1] * num
     assert layer.function(*([-1] * num)) == 0 if num == 1 else [0] * num
-    assert np.mean(x < 0.1 for x in constrained.args) > 0.5, constrained.args  # some slack to avoid flakiness
+    assert (
+        np.mean([x < 0.1 for x in constrained.args]) > 0.5
+    ), constrained.args  # some slack to avoid flakiness
