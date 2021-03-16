b9950ec3d2c3df4e6c301301bf7134c9d92cbc6c
Olivier Teytaud <oteytaud@fb.com>
2021-03-16 14:38:11 +0100

    fix_windows

--- nevergrad/functions/gymanm/test_gym_anm.py
+++ nevergrad/functions/gymanm/test_gym_anm.py
@@ -4,11 +4,13 @@
 # LICENSE file in the root directory of this source tree.
 
 import numpy as np
+from nevergrad.common import testing
 from . import gym_anm
 
 
 def test_gym_anm() -> None:
-    func = gym_anm.GymAnm()
-    x = 0 * np.random.rand(func.dimension)
-    value = func(x)  # should not touch boundaries, so value should be < np.inf
-    np.testing.assert_almost_equal(value, 1e20)
+    with testing.skip_error_on_systems(OSError, systems=("Windows",)):
+        func = gym_anm.GymAnm()
+        x = 0 * np.random.rand(func.dimension)
+        value = func(x)  # should not touch boundaries, so value should be < np.inf
+        np.testing.assert_almost_equal(value, 1e20)
