9103cd83a3cf2db83156d66017b790d2a48fc98d
Jeremy Rapin <jrapin@fb.com>
2021-04-15 16:30:25 +0200

    test

--- nevergrad/parametrization/mutation.py
+++ nevergrad/parametrization/mutation.py
@@ -14,24 +14,39 @@ from .choice import Choice
 from . import _layering
 
 
-P = tp.TypeVar("P", bound=core.Parameter)
+def _Updater:
+
+    def __init__(self, obj: "Mutation") -> None:
+        self._obj = obj
+
+    def __setitem__(self, name: str, value: tp.Any) -> None:
+        self._obj.root().parameters[name] = value
+
+    def __getitem__(self, name: str) -> tp.Any:
 
 
-class Subparameter:
-    def __init__(self, maker: tp.Callable[[], P]) -> None:
-        self.maker = maker
+class Subparameters:
+
+    def __init__(self) -> None:
+        self._init: tp.Any = None
+        self._initialized = False
         self.name = ""
 
-    def __set_name__(self, owner: "Mutation", name: str) -> None:
+    def __set_name__(self, obj: tp.Any, name: str) -> None:
         self.name = name
 
-    def __get__(self, obj: "Mutation", objtype: tp.Optional[tp.Type[object]] = None) -> core.Parameter:
-        if not self.name:
-            raise RuntimeError(f"No name provided for descriptor: {self}")
+    def __get__(self, obj: "Mutation", objtype: tp.Optional[tp.Type[object]] = None) -> "Subparameters":
         subparams = obj.root().parameters
+        if not self.name:
+            raise RuntimeError("Non-initialized descriptor")
         if self.name not in subparams:
-            subparams[self.name] = self.maker()
-        return subparams[self.name]
+            if not self._initialized:
+                raise RuntimeError("An initialization must be provided")
+            subparams[self.name] = core.as_parameter(self._init).copy()
+        return self
+
+    def __set__(self, obj: "Mutation", value: tp.Any) -> None:
+        raise AttributeError("Can't set subparameter")
 
 
 class Mutation(_layering.Layered):
@@ -45,6 +60,8 @@ class Mutation(_layering.Layered):
     Mutation should take only one p.Array instance as argument, while
     Recombinations should take several
     """
+
+    subparameters = Subparameters()
 
     def root(self) -> Data:
         data = self._layers[0]
@@ -98,6 +115,10 @@ class Crossover(Mutation):
       0 1 1 0
     """
 
+    Subparameters()
+    max_size = Subparameter()
+    fft = Subparameter()
+
     def __init__(
         self,
         axis: tp.Optional[tp.Union[int, tp.Iterable[int]]] = None,
@@ -106,16 +127,15 @@ class Crossover(Mutation):
     ) -> None:
         if not isinstance(axis, core.Parameter):
             axis = (axis,) if isinstance(axis, int) else tuple(axis) if axis is not None else None
-        super().__init__(max_size=max_size, axis=axis, fft=fft)
-
-    @property
-    def axis(self) -> tp.Optional[tp.Tuple[int, ...]]:
-        return self.parameters["axis"].value  # type: ignore
+        self.axis = Subparameter(axis)
+        self.max_size = Subparameter(max_size)
+        self.fft = Subparameter(fft)
+        super().__init__()
 
     def apply(self, arrays: tp.Sequence[Data]) -> None:
         new_value = self._apply_array([a._value for a in arrays])
         bounds = arrays[0].bounds
-        if self.parameters["fft"].value and any(x is not None for x in bounds):
+        if self.fft.value and any(x is not None for x in bounds):
             new_value = transforms.Clipping(a_min=bounds[0], a_max=bounds[1]).forward(new_value)
         arrays[0].value = new_value
 
@@ -125,7 +145,7 @@ class Crossover(Mutation):
             raise Exception("Crossover can only be applied between 2 individuals")
         transf = (
             transforms.Fourrier(range(arrays[0].dim) if self.axis is None else self.axis)
-            if self.parameters["fft"].value
+            if self.fft.value
             else None
         )
         if transf is not None:
@@ -134,7 +154,7 @@ class Crossover(Mutation):
         assert shape == arrays[1].shape, "Individuals should have the same shape"
         # settings
         axis = tuple(range(len(shape))) if self.axis is None else self.axis
-        max_size = self.parameters["max_size"].value
+        max_size = self.max_size.value
         max_size = int(((arrays[0].size + 1) / 2) ** (1 / len(axis))) if max_size is None else max_size
         max_size = min(max_size, *(shape[a] - 1 for a in axis))
         size = 1 if max_size == 1 else self.random_state.randint(1, max_size)
@@ -190,7 +210,7 @@ class Translation(Mutation):
             axis = (axis,) if isinstance(axis, int) else tuple(axis) if axis is not None else None
         super().__init__(axis=axis)
 
-    @property
+    @ property
     def axis(self) -> tp.Optional[tp.Tuple[int, ...]]:
         return self.parameters["axis"].value  # type: ignore
 
@@ -219,12 +239,12 @@ class Jumping(Mutation):
     def __init__(self, axis: int, size: int):
         super().__init__(axis=axis, size=size)
 
-    @property
+    @ property
     def axis(self) -> int:
 
         return self.parameters["axis"].value  # type: ignore
 
-    @property
+    @ property
     def size(self) -> int:
         return self.parameters["size"].value  # type: ignore
 
@@ -235,8 +255,8 @@ class Jumping(Mutation):
         size = self.random_state.randint(1, self.size)
         asdata = AxisSlicedArray(data, self.axis)
         init = self.random_state.randint(L)
-        chunck = asdata[init : init + size]
-        remain: np.ndarray = np.concatenate([asdata[:init], asdata[init + size :]], axis=self.axis)
+        chunck = asdata[init: init + size]
+        remain: np.ndarray = np.concatenate([asdata[:init], asdata[init + size:]], axis=self.axis)
         # pylint: disable=unsubscriptable-object
         newpos = self.random_state.randint(remain.shape[self.axis])
         asremain = AxisSlicedArray(remain, self.axis)
@@ -251,7 +271,7 @@ class LocalGaussian(Mutation):
             axes = (axes,) if isinstance(axes, int) else tuple(axes) if axes is not None else None
         super().__init__(axes=axes, size=size)
 
-    @property
+    @ property
     def axes(self) -> tp.Optional[tp.Tuple[int, ...]]:
         return self.parameters["axes"].value  # type: ignore
 
@@ -316,7 +336,7 @@ class TunedTranslation(Mutation):
         super().__init__(shift=Choice(range(1, shape[axis])))
         self.axis = axis
 
-    @property
+    @ property
     def shift(self) -> Choice:
         return self.parameters["shift"]  # type: ignore
 
