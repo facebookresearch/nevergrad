be2a4ac9c0bbeb2fffdb0ce7d505eb356b521183
Toky Axel <tokyaxel@localsite.mg>
2021-04-28 23:20:36 +0300

    update mixsimulator

--- nevergrad/functions/mixsimulator/core.py
+++ nevergrad/functions/mixsimulator/core.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Based on https://github.com/Foloso/MixSimulator/tree/nevergrad_experiment
+# Based on https://github.com/Foloso/MixSimulator
 
 from .. import base
 
@@ -27,10 +27,14 @@ class OptimizeMix(base.ExperimentFunction):
     def __init__(self, time: int = 8760) -> None:
         try:
             from mixsimulator.MixSimulator import MixSimulator  # pylint: disable=import-outside-toplevel
+            from mixsimulator.Demand import Demand
 
             self._mix = MixSimulator()
             self._mix.set_data_to("Toamasina")
-            
+            self._demand = Demand()
+            self._demand.set_data_to("Toamasina", delimiter=",")
+            self._mix.set_demand(self._demand)
+
         except (KeyError, AttributeError, ModuleNotFoundError) as e:
             # send a skip error so that this does not break the test suit
             raise base.UnsupportedExperiment("mixsimulator dependency issue") from e
