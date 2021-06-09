996ee92ecc0de24b4d0be4f4a3bb9ec7892f7943
Teytaud <olivier.teytaud@gmail.com>
2021-06-09 19:18:58 +0200

    Update nevergrad/functions/gym/gym_multi.py

--- nevergrad/functions/gym/gym_multi.py
+++ nevergrad/functions/gym/gym_multi.py
@@ -352,13 +352,6 @@ class GymMulti(ExperimentFunction):
             env.observation_space is not None or "ompiler" in name or "llvm" in name
         ), "An observation space should be defined."
         if "ompiler" in self.name:
-            #            assert False, f"obs={dir(env.observation_space)}    {env.observation_space.shape} {env.observation_space.sample()}"
-            #            E           AssertionError: obs=['__class__', '__contains__', '__delattr__', '__dict__', '__dir__',
-            #            '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '
-            #            __hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__',
-            #            '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__',
-            #            '__weakref__', '_np_random', 'bounded_above', 'bounded_below', 'contains', 'dtype', 'from_jsonable', 'high',
-            #            'is_bounded', 'low', 'np_random', 'sample', 'seed', 'shape', 'to_jsonable']
             input_dim = 56
             self.discrete_input = False
         elif env.observation_space is not None and env.observation_space.dtype == int:
