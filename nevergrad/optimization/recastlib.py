a09880ddb9a630735a282f629b3a55f69edd4770
Olivier Teytaud <oteytaud@fb.com>
2021-06-10 13:56:03 +0200

    newoptims

--- nevergrad/optimization/recastlib.py
+++ nevergrad/optimization/recastlib.py
@@ -26,7 +26,7 @@ class _ScipyMinimizeBase(recaster.SequentialRecastOptimizer):
         self.multirun = 1  # work in progress
         self.initial_guess: tp.Optional[tp.ArrayLike] = None
         # configuration
-        assert method in ["Nelder-Mead", "COBYLA", "SLSQP", "Powell"], f"Unknown method '{method}'"
+        assert method in ["Nelder-Mead", "COBYLA", "SLSQP", "Powell", "BB"], f"Unknown method '{method}'"
         self.method = method
         self.random_restart = random_restart
 
@@ -59,17 +59,34 @@ class _ScipyMinimizeBase(recaster.SequentialRecastOptimizer):
         remaining: float = budget - self._num_ask
         while remaining > 0:  # try to restart if budget is not elapsed
             options: tp.Dict[str, tp.Any] = {} if self.budget is None else {"maxiter": remaining}
-            res = scipyoptimize.minimize(
-                objective_function,
-                best_x if not self.random_restart else self._rng.normal(0.0, 1.0, self.dimension),
-                method=self.method,
-                options=options,
-                tol=0,
-            )
-            if res.fun < best_res:
-                best_res = res.fun
-                best_x = res.x
-            remaining = budget - self._num_ask
+            if self.method == "BB":
+                import black_box as bb
+                res = bb.search_min(f = lambda x: objective_function(np.arctanh(x)),  # given function
+                            domain = [[-1., 1.]] * self.dimension,
+                            budget = budget,  # total number of function calls available
+                            batch = 1,
+                            resfile="/dev/null")
+                best_x = np.arctanh(res)
+            elif self.method == "RBFOPT":
+                import rbfopt
+                bb = rbfopt.RbfoptUserBlackBox(d, np.array([-1.] * d), np.array([1.] * d),
+                               np.array(['R'] * d), lambda x: objective_function(np.arctanh(x)))
+                settings = rbfopt.RbfoptSettings(max_evaluations=budget)
+                alg = rbfopt.RbfoptAlgorithm(settings, bb)
+                val, x, itercount, evalcount, fast_evalcount = alg.optimize()
+                best_x = np.arctanh(x)
+            else:
+                res = scipyoptimize.minimize(
+                    objective_function,
+                    best_x if not self.random_restart else self._rng.normal(0.0, 1.0, self.dimension),
+                    method=self.method,
+                    options=options,
+                    tol=0,
+                )
+                if res.fun < best_res:
+                    best_res = res.fun
+                    best_x = res.x
+                remaining = budget - self._num_ask
         return best_x
 
 
@@ -87,6 +104,8 @@ class ScipyOptimizer(base.ConfiguredOptimizer):
         - SQP (or SLSQP): very powerful e.g. in continuous noisy optimization. It is based on
           approximating the objective function by quadratic models.
         - Powell
+        - BB
+        - RBFOPT
     random_restart: bool
         whether to restart at a random point if the optimizer converged but the budget is not entirely
         spent yet (otherwise, restarts from best point)
@@ -105,6 +124,8 @@ class ScipyOptimizer(base.ConfiguredOptimizer):
 
 
 NelderMead = ScipyOptimizer(method="Nelder-Mead").set_name("NelderMead", register=True)
+BB = ScipyOptimizer(method="BB").set_name("BB", register=True)
+RBFOPT = ScipyOptimizer(method="RBFOPT").set_name("RBFOPT", register=True)
 Powell = ScipyOptimizer(method="Powell").set_name("Powell", register=True)
 RPowell = ScipyOptimizer(method="Powell", random_restart=True).set_name("RPowell", register=True)
 Cobyla = ScipyOptimizer(method="COBYLA").set_name("Cobyla", register=True)
