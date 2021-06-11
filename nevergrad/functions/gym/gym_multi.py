aa9bd8de049869477440cf0d500f4373210b288b
Olivier Teytaud <oteytaud@fb.com>
2021-06-11 15:40:03 +0200

    fix

--- nevergrad/functions/gym/gym_multi.py
+++ nevergrad/functions/gym/gym_multi.py
@@ -577,7 +577,7 @@ class GymMulti(ExperimentFunction):
                         ),
                     )
                 )
-                for compiler_gym_pb_index in np.random.choice(range(0, self.num_training_codes), 30)
+                for compiler_gym_pb_index in np.random.choice(range(0, self.num_training_codes), size=100, replace=False)
             ]
             return -np.exp(np.sum(log_rewards) / len(log_rewards))
 
