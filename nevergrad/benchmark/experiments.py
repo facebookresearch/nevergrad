66401ececa07be2379dfa9d732d9dab3f4f461e4
Teytaud <olivier.teytaud@gmail.com>
2021-06-16 09:46:42 +0200

    Add complex tsp (#1015)
    * complex tsp
    
    * Update test_core.py
    
    * Update experiments.py
    
    * Update core.py
    
    * Update core.py
    
    * Update experiments.py
    
    * Update test_core.py
    
    * Update nevergrad/benchmark/experiments.py
    
    Cool yes.
    
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>
    
    * fix
    
    * fix
    
    * fix_jrapin
    
    * Update nevergrad/functions/stsp/core.py
    
    * remove useless suffix
    
    * remove pointless suffix
    
    * remove pointless suffix
    
    * complex_tsp
    
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -1226,7 +1226,7 @@ def neuro_control_problem(seed: tp.Optional[int] = None) -> tp.Iterator[Experime
 
 
 @registry.register
-def simpletsp(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
+def simple_tsp(seed: tp.Optional[int] = None, complex_tsp: bool = False) -> tp.Iterator[Experiment]:
     """Simple TSP problems. Please note that the methods we use could be applied or complex variants, whereas
     specialized methods can not always do it; therefore this comparisons from a black-box point of view makes sense
     even if white-box methods are not included though they could do this more efficiently.
@@ -1234,7 +1234,7 @@ def simpletsp(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
     Budgets doubling from 25, 50, 100, 200, ... up  to 25600
 
     """
-    funcs = [STSP(10), STSP(100), STSP(1000), STSP(10000)]
+    funcs = [STSP(10 ** k, complex_tsp) for k in range(2, 6)]
     seedg = create_seed_generator(seed)
     optims = get_optimizers("basics", "noisy", seed=next(seedg))
     for budget in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600]:
@@ -1245,6 +1245,12 @@ def simpletsp(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
                         xp = Experiment(fu, algo, budget, num_workers=num_workers, seed=next(seedg))
                         if not xp.is_incoherent:
                             yield xp
+
+
+@registry.register
+def complex_tsp(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
+    """Counterpart of simple_tsp with non-planar term."""
+    return simple_tsp(seed, complex_tsp=True)
 
 
 @registry.register
