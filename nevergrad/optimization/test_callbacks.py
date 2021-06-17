130470773546a641fca65195cda03a8c64f17765
Olivier Teytaud <oteytaud@fb.com>
2021-06-17 18:45:34 +0200

    no_idea_what_i_am_doing

--- nevergrad/optimization/test_callbacks.py
+++ nevergrad/optimization/test_callbacks.py
@@ -35,7 +35,7 @@ def test_log_parameters(tmp_path: Path) -> None:
     assert isinstance(logs[-1]["1"], float)
     assert len(logs[-1]) == 31
     logs = logger.load_flattened(max_list_elements=2)
-    assert len(logs[-1]) == 25
+    assert len(logs[-1]) == 27
     # deletion
     logger = callbacks.ParametersLogger(filepath, append=False)
     assert not logger.load()
