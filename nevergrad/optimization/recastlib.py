750b4335e29ee254bcf011fd98b2f39da79de16a
Olivier Teytaud <oteytaud@fb.com>
2021-06-22 08:10:30 +0200

    fix

--- nevergrad/optimization/recastlib.py
+++ nevergrad/optimization/recastlib.py
@@ -66,8 +66,7 @@ class _ScipyMinimizeBase(recaster.SequentialRecastOptimizer):
         while remaining > 0:  # try to restart if budget is not elapsed
             options: tp.Dict[str, tp.Any] = {} if self.budget is None else {"maxiter": remaining}
             if self.method == "SMAC":
-                import smac
-                from scipy.optimize._shgo_lib.sobol_seq import Sobol  # noqa  # pylint: disable=unused-import
+                import smac  # noqa  # pylint: disable=unused-import
                 from smac.facade.func_facade import fmin_smac  # noqa  # pylint: disable=unused-import
 
                 x, cost, _ = fmin_smac(
