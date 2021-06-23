161cdebfb77284fb0d36b1e8a76309a2baf11afd
Jérémy Rapin <jrapin.github@gmail.com>
2021-06-23 13:52:52 +0200

    Update typing for new numpy version (#1164)
    * Update typing for new numpy version
    
    * fix
    
    Co-authored-by: Jeremy Rapin <jrapin@fb.com>

--- nevergrad/common/typing.py
+++ nevergrad/common/typing.py
@@ -49,7 +49,7 @@ ArrayLike = Union[Tuple[float, ...], List[float], _np.ndarray]
 PathLike = Union[str, Path]
 FloatLoss = float
 Loss = Union[float, ArrayLike]
-BoundValue = Optional[Union[float, int, _np.int_, _np.float_, _np.ndarray]]
+BoundValue = Optional[Union[float, int, _np.int_, _np.float_, ArrayLike]]
 
 
 # %% Protocol definitions for executor typing
