7ad265f4f1b4f0b2c81df26b6aeada3aeaf54ace
Olivier Teytaud <oteytaud@fb.com>
2021-07-05 13:05:36 +0200

    mypyerror

--- nevergrad/optimization/base.py
+++ nevergrad/optimization/base.py
@@ -449,7 +449,7 @@ class Optimizer:  # pylint: disable=too-many-instance-attributes
             else:
                 try:  # Sometimes we have a limited budget so that
                     candidate = self._internal_ask_candidate()
-                except AssertionError:
+                except AssertionError as e:
                     assert (
                         self.parametrization._constraint_checkers
                     ), f"Error: {e}"  # This should not happen without constraint issues.
