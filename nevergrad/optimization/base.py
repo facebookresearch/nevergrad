95e2be9b6b4aa44fd87d7ec95ab050b1d785509a
Teytaud <olivier.teytaud@gmail.com>
2021-07-05 12:04:14 +0200

    Update base.py

--- nevergrad/optimization/base.py
+++ nevergrad/optimization/base.py
@@ -441,7 +441,6 @@ class Optimizer:  # pylint: disable=too-many-instance-attributes
         # - no memory of previous iterations.
         # - just projection to constraint satisfaction.
         # We try using the normal tool during half constraint budget, in order to reduce the impact on the normal run.
-        constraint_issue = False
         for _ in range(max_trials):
             is_suggestion = False
             if self._suggestions:  # use suggestions if available
@@ -451,13 +450,10 @@ class Optimizer:  # pylint: disable=too-many-instance-attributes
                 try:  # Sometimes we have a limited budget so that
                     candidate = self._internal_ask_candidate()
                 except AssertionError:
-                    assert constraint_issue  # This should not happen without constraint issues.
+                    assert self.parametrization._constraint_checker  # This should not happen without constraint issues.
                     candidate = self.parametrization.spawn_child()
             if candidate.satisfies_constraints():
                 break  # good to go!
-            constraint_issue = (
-                True  # It is normal that we exceed the sampler budget -- constraint satisfaction.
-            )
             if self._penalize_cheap_violations:
                 # Warning! This might be a tell not asked.
                 self._internal_tell_candidate(candidate, float("Inf"))  # DE requires a tell
