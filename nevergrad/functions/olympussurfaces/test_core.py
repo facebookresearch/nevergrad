82ed9d8b6eb0f703063bca5ef4d0ac94bafac3a2
Olivier Teytaud <oteytaud@fb.com>
2021-07-15 08:39:43 +0200

    fix

--- nevergrad/functions/olympussurfaces/test_core.py
+++ nevergrad/functions/olympussurfaces/test_core.py
@@ -3,7 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
 import numpy as np
+from unittest import SkipTest
 from . import core
 import pytest
 
