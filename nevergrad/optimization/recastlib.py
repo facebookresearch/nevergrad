29b8126db8962a9b6919d1b6e526487bbba79b33
Theodore Clarke <theoajclarke@devfair0149.h2.fair>
2021-07-28 07:43:36 -0700

    inline imports for pymoo optional dependency

--- nevergrad/optimization/recastlib.py
+++ nevergrad/optimization/recastlib.py
@@ -276,7 +276,7 @@ class PymooOptimizer(base.ConfiguredOptimizer):
 # PymooGA = PymooOptimizer(algorithm=get_pymoo_algorithm("ga")).set_name("ga", register=True)
 # PymooBRKGA = PymooOptimizer(algorithm=get_pymoo_algorithm("brkga")).set_name("brkga", register=True)
 # PymooCMAES = PymooOptimizer(algorithm=get_pymoo_algorithm("cmaes")).set_name("cmaes", register=True)
-# PymooNSGA2 = PymooOptimizer(algorithm="nsga2").set_name("PymooNSGA2", register=True)
+PymooNSGA2 = PymooOptimizer(algorithm="nsga2").set_name("PymooNSGA2", register=True)
 # PymooRNSGA2 = PymooOptimizer(algorithm="rnsga2").set_name("PymooRNSGA2", register=True)
 # PymooNSGA3 = PymooOptimizer(algorithm="nsga3").set_name("PymooNSGA3", register=True)
 # PymooUNSGA3 = PymooOptimizer(algorithm="unsga3").set_name("PymooUNSGA3", register=True)
