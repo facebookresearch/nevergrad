cc3b62ca0cb4c9494815a27f48c738742bfd541b
Theodore Clarke <theoajclarke@devfair0149.h2.fair>
2021-08-03 03:59:35 -0700

    updated optimization function types

--- nevergrad/optimization/recaster.py
+++ nevergrad/optimization/recaster.py
@@ -170,7 +170,7 @@ class RecastOptimizer(base.Optimizer):
         self._messaging_thread: tp.Optional[MessagingThread] = None  # instantiate at runtime
         self._last_optimizer_duration = 0.0001
 
-    def get_optimization_function(self) -> tp.Callable[[tp.Callable[..., tp.Any]], tp.ArrayLike]:
+    def get_optimization_function(self) -> tp.Callable[[tp.Callable[..., tp.Any]], tp.Optional[tp.ArrayLike]]:
         """Return an optimization procedure function (taking a function to optimize as input)
 
         Note
@@ -234,7 +234,7 @@ class RecastOptimizer(base.Optimizer):
         messages = [m for m in messages if m.meta["uid"] == candidate.uid]
         if not messages:
             raise RuntimeError(f"No message for evaluated point {x}: {self._messaging_thread.messages}")
-        messages[0].result = loss  # post the value, and the thread will deal with it
+        messages[0].result = candidate.loss  # post the value, and the thread will deal with it
 
     def _internal_tell_not_asked(self, candidate: p.Parameter, loss: float) -> None:
         raise base.errors.TellNotAskedNotSupportedError
