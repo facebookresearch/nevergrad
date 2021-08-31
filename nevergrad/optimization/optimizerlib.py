242026cbd6817de3e869340b0d08e5ee646e5a75
Elena Raponi <elenaraponi@fb.com>
2021-08-31 02:30:30 -0700

    Restore NGOpt14

--- nevergrad/optimization/optimizerlib.py
+++ nevergrad/optimization/optimizerlib.py
@@ -2647,7 +2647,7 @@ class NGOpt13(NGOpt12):  # Also known as NGOpt12H
 class NGOpt14(NGOpt12):  # Also known as NGOpt12H_nohyperopt
     def _select_optimizer_cls(self) -> base.OptCls:
         if self.budget is not None and self.budget < 600:
-            return MetaModelOpO
+            return MetaModel
         else:
             return super()._select_optimizer_cls()
 
