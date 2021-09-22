2056aa8a64e4dfa04d5103830827a2f240f62e63
Jeremy Rapin <jrapin@fb.com>
2021-09-22 14:42:39 +0200

    Integrate #1176

--- nevergrad/optimization/test_special.py
+++ nevergrad/optimization/test_special.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import os
+import time
+import collections
 import typing as tp
 import pytest
 import numpy as np
@@ -15,38 +17,42 @@ if not os.environ.get(KEY, ""):
     pytest.skip(f"These tests only run if {KEY} is set in the environment", allow_module_level=True)
 
 
-@pytest.mark.parametrize("dimension", (7,))
+@pytest.mark.parametrize("dimension", (2, 4, 7, 77))
 @pytest.mark.parametrize("num_workers", (1,))
-@pytest.mark.parametrize("scale", (8.0,))
+@pytest.mark.parametrize("scale", (4.0,))
 @pytest.mark.parametrize("baseline", ["MetaModel", "CMA", "ECMA"])
-@pytest.mark.parametrize("budget", [200, 500])
+@pytest.mark.parametrize("budget", [400, 4000])
 @pytest.mark.parametrize("ellipsoid", [True, False])
 def test_metamodel_sqp_chaining(
     dimension: int, num_workers: int, scale: float, budget: int, ellipsoid: bool, baseline: str
 ) -> None:
     """The test can operate on the sphere or on an elliptic funciton."""
     target = test_optimizerlib.QuadFunction(scale=scale, ellipse=ellipsoid)
+    baseline = baseline if dimension > 1 else "OnePlusOne"
+    chaining = "ChainMetaModelSQP"
 
     # In both cases we compare MetaModel and CMA for a same given budget.
     # But we expect MetaModel to be clearly better only for a larger budget in the ellipsoid case.
     contextual_budget = budget if ellipsoid else 3 * budget
     contextual_budget *= 5 * int(max(1, np.sqrt(scale)))
 
-    num_trials = 15
+    num_trials = 27
     successes = 0.0
+    durations: tp.Dict[str, float] = collections.defaultdict(int)
     for _ in range(num_trials):
         if successes >= num_trials / 2:
             break
         # Let us run the comparison.
-        recommendations: tp.List[np.ndarray] = []
-        for name in ("ChainMetaModelSQP", baseline if dimension > 1 else "OnePlusOne"):
+        recoms: tp.Dict[str, np.ndarray] = {}
+        for name in (chaining, baseline):
             opt = registry[name](dimension, contextual_budget, num_workers=num_workers)
-            recommendations.append(opt.minimize(target).value)
-        chaining_recom, default_recom = recommendations  # pylint: disable=unbalanced-tuple-unpacking
+            t0 = time.time()
+            recoms[name] = opt.minimize(target).value
+            durations[name] += time.time() - t0
 
-        if target(default_recom) < target(chaining_recom):
+        if target(recoms[baseline]) < target(recoms[chaining]):
             successes += 1
-        if target(default_recom) == target(chaining_recom):
+        if target(recoms[baseline]) == target(recoms[chaining]):
             successes += 0.5
 
     if successes <= num_trials // 2:
@@ -54,11 +60,12 @@ def test_metamodel_sqp_chaining(
             f"ChainMetaModelSQP fails ({successes}/{num_trials}) for d={dimension}, scale={scale}, "
             f"num_workers={num_workers}, ellipsoid={ellipsoid}, budget={budget}, vs {baseline}"
         )
-        assert False, "ChaingMetaModelSQP fails."
+        raise AssertionError("ChaingMetaModelSQP fails by performance.")
     print(
         f"ChainMetaModelSQP wins for d={dimension}, scale={scale}, num_workers={num_workers}, "
         f"ellipsoid={ellipsoid}, budget={budget}, vs {baseline}"
     )
+    assert durations[chaining] < 7 * durations[baseline], "Computationally more than 7x more expensive."
 
 
 @pytest.mark.parametrize("args", test_optimizerlib.get_metamodel_test_settings(special=True))
