430160809b69604d8675161a674d4110ddbc2c62
Olivier Teytaud <oteytaud@fb.com>
2021-10-01 09:29:11 +0200

    fix

--- nevergrad/optimization/recastlib.py
+++ nevergrad/optimization/recastlib.py
@@ -8,6 +8,7 @@ import math
 import warnings
 import numpy as np
 from scipy import optimize as scipyoptimize
+from unittest import SkipTest
 import nevergrad.common.typing as tp
 from nevergrad.parametrization import parameter as p
 from nevergrad.common import errors
