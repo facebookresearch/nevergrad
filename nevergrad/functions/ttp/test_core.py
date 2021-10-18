22cbc030e65113f639b1dfe5320ab869e5beae31
a1769504 <a1769504@student.adelaide.edu.au>
2021-10-18 11:16:08 +1030

    add unit test

---
+++ nevergrad/functions/ttp/test_core.py
@@ -0,0 +1,19 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import typing as tp
+from unittest.mock import patch
+import numpy as np
+from . import core
+
+
+def test_ttp() -> None:
+    np.random.seed(17)
+
+    func = core.TTPInstance()
+    tsp_tour = np.random.random_sample((19,))
+    packing_plan = np.random.random_sample((95,))
+    value = func.function(tsp_tour, packing_plan)
+    np.testing.assert_almost_equal(value, 15740.25)
