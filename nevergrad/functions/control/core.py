840693c6751c5954a9411147847bafe5fd9289d8
Teytaud <olivier.teytaud@gmail.com>
2021-10-20 08:48:11 +0200

    Update core.py

--- nevergrad/functions/control/core.py
+++ nevergrad/functions/control/core.py
@@ -92,8 +92,9 @@ class BaseFunction(base.ExperimentFunction):
             noise_level=self.noise_level,
             deterministic_sim=deterministic_sim,
         )
-        if self.noise_level > 0.0 or not deterministic_sim:
-            self.parametrization.function.deterministic = False
+        #This was interesting, but the noise level in mujoco is not sufficient for making this flag useful: 
+        #if self.noise_level > 0.0 or not deterministic_sim:
+        #    self.parametrization.function.deterministic = False
         self._descriptors.pop("random_state", None)  # remove it from automatically added descriptors
 
     def _simulate(self, x: tp.Tuple) -> float:
