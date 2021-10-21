e1b0be02e0937f7c6c767e27aefe68ebeb244b98
Olivier Teytaud <oteytaud@fb.com>
2021-10-21 22:28:25 +0200

    bouhououh_yet_another_stupid_bug

--- nevergrad/benchmark/gymexperiments.py
+++ nevergrad/benchmark/gymexperiments.py
@@ -76,7 +76,7 @@ def ng_full_gym(
             print("pip install pyvirtualdisplay")
             print("pip install git+https://github.com/benelot/pybullet-gym")
             if os.environ.get("CIRCLECI", False):
-                ng.errors.UnsupportedExperiment("No pybullet in CircleCI because pybulletgym is not in pypi!")
+                raise ng.errors.UnsupportedExperiment("No pybullet in CircleCI because pybulletgym is not in pypi!")
             raise ImportError("Please install pybullet, pyvirtualdisplay and pybulletgym.")
 
     seedg = create_seed_generator(seed)
