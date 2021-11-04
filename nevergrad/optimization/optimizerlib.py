883c1af2a8892cc733167f8056ac7292c63e08cb
Teytaud <olivier.teytaud@gmail.com>
2021-11-04 13:55:07 +0100

    Update nevergrad/optimization/optimizerlib.py
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>

--- nevergrad/optimization/optimizerlib.py
+++ nevergrad/optimization/optimizerlib.py
@@ -402,10 +402,9 @@ class _CMA(base.Optimizer):
 
     @property
     def es(self) -> tp.Any:  # typing not possible since cmaes not imported :(
+        scale_multiplier = 1.0
         if p.helpers.Normalizer(self.parametrization).fully_bounded:
-            scale_multiplier = 0.3 if self.dimension < 18 else 0.15
-        else:
-            scale_multiplier = 1.0
+            scale_multiplier = 0.3 if self.dimension < 18 else 0.15       
         if self._es is None:
             if not self._config.fcmaes:
                 import cma  # import inline in order to avoid matplotlib initialization warning
