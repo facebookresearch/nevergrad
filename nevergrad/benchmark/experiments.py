461c47a7af914f70444039a1007602d572faabed
Teytaud <olivier.teytaud@gmail.com>
2022-01-21 15:01:54 +0100

    Update experiments.py

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -1181,7 +1181,8 @@ def rocket(seed: tp.Optional[int] = None, seq: bool = False) -> tp.Iterator[Expe
 def pcse(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
     """Crop simulator.
 
-    Low dimensional problem, only 2 vars.
+    Low dimensional problem, only 2 vars. This is optimization for model identification: we want
+    to find parameters so that the simulation matches observations.
     """
     funcs = [Pcse()]
     seedg = create_seed_generator(seed)
