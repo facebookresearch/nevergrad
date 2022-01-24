004085c41d48040c3cf228870a7e44a44b3545c3
Teytaud <olivier.teytaud@gmail.com>
2022-01-24 21:13:27 +0100

    Update gymexperiments.py

--- nevergrad/benchmark/gymexperiments.py
+++ nevergrad/benchmark/gymexperiments.py
@@ -150,7 +150,7 @@ def ng_full_gym(
         assert not multi
     if conformant:
         controls = ["stochastic_conformant"]
-    budgets = [204800, 12800, 25600, 51200, 50, 200, 800, 3200, 6400, 100, 25, 400, 1600, 102400]
+    budgets = [25, 50, 100, 200, 400, 800, 1600, 3200, 6400]
     budgets = gym_budget_modifier(budgets)
     for control in controls:
         neural_factors: tp.Any = (
