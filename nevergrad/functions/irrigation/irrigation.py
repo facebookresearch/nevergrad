e7ddbd390fe29591ac3d83be5a25a621e114493f
Olivier Teytaud <oteytaud@fb.com>
2022-03-02 19:07:20 +0100

    fix

--- nevergrad/functions/irrigation/irrigation.py
+++ nevergrad/functions/irrigation/irrigation.py
@@ -11,8 +11,10 @@ https://raw.githubusercontent.com/purdue-orbital/pcse-simulation/master/Simulati
 
 
 from pathlib import Path
+import os
 import urllib.request
 import numpy as np
+import nevergrad as ng
 from ..base import ArrayExperimentFunction
 
 # pylint: disable=too-many-locals,too-many-statements
@@ -62,6 +64,8 @@ def leaf_area_index(x: np.ndarray):
     print("PCSE version: %s" % pcse.__version__)
 
     crop = YAMLCropDataProvider()
+    if os.environ.get("CIRCLECI", False):
+        raise ng.errors.UnsupportedExperiment("No HTTP request in CircleCI")
     urllib.request.urlretrieve(
         "https://raw.githubusercontent.com/ajwdewit/ggcmi/master/pcse/doc/ec3.soil",
         str(data_dir) + "/soil/ec3.soil",
