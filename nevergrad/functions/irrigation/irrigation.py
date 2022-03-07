930058dffa354c68b73738cc425edcc612e8d51f
Olivier Teytaud <oteytaud@fb.com>
2022-03-07 10:37:26 +0100

    fix

--- nevergrad/functions/irrigation/irrigation.py
+++ nevergrad/functions/irrigation/irrigation.py
@@ -11,7 +11,6 @@ https://raw.githubusercontent.com/purdue-orbital/pcse-simulation/master/Simulati
 
 
 from pathlib import Path
-<<<<<<< HEAD
 import urllib.request  # Necessary for people who will uncomment the part using data under EUPL license.
 import numpy as np
 import nevergrad as ng
@@ -25,36 +24,33 @@ from pcse.db import NASAPowerWeatherDataProvider
 from pcse.util import WOFOST72SiteDataProvider
 from pcse.base import ParameterProvider
 
-=======
-import urllib.request
-import numpy as np
-import nevergrad as ng
-from ..base import ArrayExperimentFunction
->>>>>>> c3c99b9c (ZZPcse2: irrigation challenge (#1341))
 
 # pylint: disable=too-many-locals,too-many-statements
 
 
 class Irrigation(ArrayExperimentFunction):
-<<<<<<< HEAD
     variant_choice = {}
     def __init__(self, symmetry: int) -> None:
         data_dir = Path(__file__).with_name("data")
-        #urllib.request.urlretrieve(
-        #    "https://raw.githubusercontent.com/ajwdewit/pcse_notebooks/master/data/soil/ec3.soil",
-        #    str(data_dir) + "/soil/ec3.soil",
-        #)
-        self.soil = CABOFileReader(os.path.join(data_dir, "soil", "ec3.soil"))
+        try:
+            self.soil = CABOFileReader(os.path.join(data_dir, "soil", "ec3.soil"))
+        except:
+            urllib.request.urlretrieve(
+                "https://raw.githubusercontent.com/ajwdewit/pcse_notebooks/master/data/soil/ec3.soil",
+                str(data_dir) + "/soil/ec3.soil",
+            )
+            self.soil = CABOFileReader(os.path.join(data_dir, "soil", "ec3.soil"))
         param = ng.p.Array(shape=(8,), lower=(0.0), upper=(1.0)).set_name("irrigation8")
         super().__init__(self.leaf_area_index, parametrization=param, symmetry=symmetry)
         if os.environ.get("CIRCLECI", False):
             raise ng.errors.UnsupportedExperiment("No HTTP request in CircleCI")
         known_longitudes = {'Saint-Leger-Bridereix': 1.5887348, 'Dun-Le-Palestel': 1.6641173, 'Kolkata':
         88.35769124388872, 'Antananarivo': 47.5255809, 'Santiago': -70.6504502, 'Lome': 1.215829, 'Cairo': 31.2357257,
-        'Ouagadougou': -1.5270944, 'Yamoussoukro': -5.273263, 'Yaounde': 11.5213344, 'Kiev': 30.5241361}
+        'Ouagadougou': -1.5270944, 'Yamoussoukro': -5.273263, 'Yaounde': 11.5213344, 'Kiev': 30.5241361, 'Porto-Novo':
+        2.6289}
         known_latitudes = {'Saint-Leger-Bridereix': 46.2861759, 'Dun-Le-Palestel': 46.3052049, 'Kolkata': 22.5414185,
         'Antananarivo': -18.9100122, 'Santiago': -33.4377756, 'Lome': 6.130419, 'Cairo': 30.0443879, 'Ouagadougou':
-        12.3681873, 'Yamoussoukro': 6.809107, 'Yaounde': 3.8689867, 'Kiev': 50.4500336}
+        12.3681873, 'Yamoussoukro': 6.809107, 'Yaounde': 3.8689867, 'Kiev': 50.4500336, 'Porto-Novo': 6.4969}
         self.cropd = YAMLCropDataProvider()
         for k in range(1000):
             if symmetry in self.variant_choice and k < self.variant_choice[symmetry]:
@@ -71,6 +67,7 @@ class Irrigation(ArrayExperimentFunction):
                     "Ouagadougou",
                     "Yamoussoukro",
                     "Yaounde",
+                    "Porto-Novo",
                     "Kiev",
                 ]
             )
@@ -93,7 +90,8 @@ class Irrigation(ArrayExperimentFunction):
 
 
     def set_data(self, symmetry: int, k: int):
-        crop_types = [c for c in self.cropd.crop_types if "obacco" not in c]
+        crop_types = [crop for crop, variety in self.cropd.get_crops_varieties().items()]
+        crop_types = [c for c in crop_types if "obacco" not in c]
         self.cropname = np.random.RandomState(symmetry+3*k+1).choice(crop_types)
         self.cropvariety = np.random.RandomState(symmetry+3*k+2).choice(list(self.cropd.get_crops_varieties()[self.cropname])
         )
@@ -108,10 +106,10 @@ class Irrigation(ArrayExperimentFunction):
         d1 = int(1.01 + 30.98 * x[1])
         d2 = int(1.01 + 30.98 * x[2])
         d3 = int(1.01 + 29.98 * x[3])
-        a0 = 15.0 * x[4] / (x[4] + x[5] + x[6] + x[7])
-        a1 = 15.0 * x[5] / (x[4] + x[5] + x[6] + x[7])
-        a2 = 15.0 * x[6] / (x[4] + x[5] + x[6] + x[7])
-        a3 = 15.0 * x[7] / (x[4] + x[5] + x[6] + x[7])
+        a0 = x[4]  #15.0 * x[4] / (x[4] + x[5] + x[6] + x[7])
+        a1 = x[5]  #15.0 * x[5] / (x[4] + x[5] + x[6] + x[7])
+        a2 = x[6]  #15.0 * x[6] / (x[4] + x[5] + x[6] + x[7])
+        a3 = x[7]  #15.0 * x[7] / (x[4] + x[5] + x[6] + x[7])
 
         yaml_agro = f"""
         - 2006-01-01:
@@ -150,108 +148,3 @@ class Irrigation(ArrayExperimentFunction):
         df.tail()
 
         return -sum([float(o["LAI"]) for o in output if o["LAI"] is not None])
-=======
-    def __init__(self, symmetry: int) -> None:
-        import nevergrad as ng
-
-        param = ng.p.Array(shape=(8,), lower=(0.0), upper=(1.0)).set_name("irrigation8")
-        super().__init__(leaf_area_index, parametrization=param, symmetry=symmetry)
-
-
-import numpy as np
-
-
-def leaf_area_index(x: np.ndarray):
-    d0 = int(1.01 + 29.98 * x[0])
-    d1 = int(1.01 + 30.98 * x[1])
-    d2 = int(1.01 + 30.98 * x[2])
-    d3 = int(1.01 + 29.98 * x[3])
-    a0 = 15.0 * x[4] / (x[4] + x[5] + x[6] + x[7])
-    a1 = 15.0 * x[5] / (x[4] + x[5] + x[6] + x[7])
-    a2 = 15.0 * x[6] / (x[4] + x[5] + x[6] + x[7])
-    a3 = 15.0 * x[7] / (x[4] + x[5] + x[6] + x[7])
-    import os, sys
-
-    # import matplotlib
-    # matplotlib.style.use("ggplot")
-    # import matplotlib.pyplot as plt
-    import pandas as pd
-    import yaml
-
-    try:
-        import pcse
-    except:
-        raise ng.errors.UnsupportedExperiment("You need to install PCSE. Not done in CircleCI.")
-    from pcse.models import Wofost72_WLP_FD
-    from pcse.fileinput import CABOFileReader, YAMLCropDataProvider
-
-    # from pcse.db import NASAPowerWeatherDataProvider
-    from pcse.util import WOFOST72SiteDataProvider
-    from pcse.base import ParameterProvider
-
-    # data_dir = os.path.join(os.getcwd(), "data")
-    data_dir = Path(__file__).with_name("data")
-
-    print("This notebook was built with:")
-    print("python version: %s " % sys.version)
-    print("PCSE version: %s" % pcse.__version__)
-
-    crop = YAMLCropDataProvider()
-    if os.environ.get("CIRCLECI", False):
-        raise ng.errors.UnsupportedExperiment("No HTTP request in CircleCI")
-    urllib.request.urlretrieve(
-        "https://raw.githubusercontent.com/ajwdewit/ggcmi/master/pcse/doc/ec3.soil",
-        str(data_dir) + "/soil/ec3.soil",
-    )
-    soil = CABOFileReader(os.path.join(data_dir, "soil", "ec3.soil"))
-    site = WOFOST72SiteDataProvider(WAV=100, CO2=360)
-    parameterprovider = ParameterProvider(soildata=soil, cropdata=crop, sitedata=site)
-
-    crop = YAMLCropDataProvider()
-
-    from pcse.fileinput import ExcelWeatherDataProvider
-
-    weatherfile = os.path.join(data_dir, "meteo", "nl1.xlsx")
-    weatherdataprovider = ExcelWeatherDataProvider(weatherfile)
-
-    yaml_agro = f"""
-    - 2006-01-01:
-        CropCalendar:
-            crop_name: sugarbeet
-            variety_name: Sugarbeet_603
-            crop_start_date: 2006-03-31
-            crop_start_type: emergence
-            crop_end_date: 2006-10-20
-            crop_end_type: harvest
-            max_duration: 300
-        TimedEvents:
-        -   event_signal: irrigate
-            name: Irrigation application table
-            comment: All irrigation amounts in cm
-            events_table:
-            - 2006-06-{d0:02}: {{amount: {a0}, efficiency: 0.7}}
-            - 2006-07-{d1:02}: {{amount: {a1}, efficiency: 0.7}}
-            - 2006-08-{d2:02}: {{amount: {a2}, efficiency: 0.7}}
-            - 2006-09-{d3:02}: {{amount: {a3}, efficiency: 0.7}}
-        StateEvents: null
-    """
-    try:
-        agromanagement = yaml.safe_load(yaml_agro)
-        wofost = Wofost72_WLP_FD(parameterprovider, weatherdataprovider, agromanagement)
-        wofost.run_till_terminate()
-    except Exception as e:
-        assert False, f"Problem!\n Dates: {d0} {d1} {d2} {d3},\n amounts: {a0}, {a1}, {a2}, {a3}\n  ({e}).\n"
-        raise e
-
-    output = wofost.get_output()
-    df = pd.DataFrame(output).set_index("day")
-    df.tail()
-
-    # print(output)
-    # print(len(output))
-    return -sum([float(o["LAI"]) for o in output if o["LAI"] is not None])
-    # fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(16,8))
-    # df['LAI'].plot(ax=axes[0], title="Leaf Area Index")
-    # df['SM'].plot(ax=axes[1], title="Root zone soil moisture")
-    # fig.autofmt_xdate()
->>>>>>> c3c99b9c (ZZPcse2: irrigation challenge (#1341))
