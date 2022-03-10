1d1cb80046c04603fee1965fcca2910297c68d10
Olivier Teytaud <oteytaud@fb.com>
2022-03-10 11:15:25 +0100

    black

--- nevergrad/functions/irrigation/__init__.py
+++ nevergrad/functions/irrigation/__init__.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 # Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
-=======
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
->>>>>>> 1691eaa4 (Pcse2: irrigation challenge (#1341))
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
