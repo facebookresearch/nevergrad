c605a47691d796ac20633fffd7f8a3b68beff66b
Teytaud <olivier.teytaud@gmail.com>
2022-03-11 14:38:52 +0100

    Reduce noise in gym (#1333)
    * Reduce noise in gym
    
    * Update multigym.py
    
    * Add comment

--- nevergrad/functions/gym/multigym.py
+++ nevergrad/functions/gym/multigym.py
@@ -419,6 +419,7 @@ class GymMulti(ExperimentFunction):
     ) -> None:
         # limited_compiler_gym: bool or None.
         #        whether we work with the limited version
+        self.num_calls = 0
         self.limited_compiler_gym = limited_compiler_gym
         self.compilergym_index = compiler_gym_pb_index
         self.optimization_scale = optimization_scale
@@ -612,15 +613,19 @@ class GymMulti(ExperimentFunction):
             assert not self.uses_compiler_gym
             return self.gym_multi_function(x, limited_fidelity=False)
         if not self.uses_compiler_gym:
+            # We want to reduce noise by averaging without
+            # spending more than 20% of the whole experiment,
+            # hence the line below:
+            num = max(self.num_calls // 5, 23)
             # Pb_index >= 0 refers to the test set.
             return (
                 np.sum(
                     [
                         self.gym_multi_function(x, limited_fidelity=False)
-                        for compiler_gym_pb_index in range(23)
+                        for compiler_gym_pb_index in range(num)
                     ]
                 )
-                / 23.0  # This is not compiler_gym but we keep this 23 constant.
+                / num  # This is not compiler_gym but we keep this 23 constant.
             )
         assert self.uses_compiler_gym
         rewards = [
@@ -750,6 +755,7 @@ class GymMulti(ExperimentFunction):
             compiler_gym_pb_index: int or None.
                 index of the compiler_gym pb: set only for testing
         """
+        self.num_calls += 1
         # Deterministic conformant: do  the average of 7 simullations always with the same seed.
         # Otherwise: apply a random seed and do a single simulation.
         train_set = compiler_gym_pb_index is None
