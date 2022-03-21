e1393eea66c4f3b69077802a0582d746d8393522
Teytaud <olivier.teytaud@gmail.com>
2022-03-21 17:02:53 +0100

    Update nevergrad/functions/cycling/__init__.py
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>

--- nevergrad/functions/cycling/__init__.py
+++ nevergrad/functions/cycling/__init__.py
@@ -3,4 +3,5 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .cycling import cycling as cycling
+from .cycling import Cycling as Cycling
+
