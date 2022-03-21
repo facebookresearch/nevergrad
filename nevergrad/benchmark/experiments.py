07f6ec0d0669739f764171a534a16e496754bc0f
Teytaud <olivier.teytaud@gmail.com>
2022-03-21 18:24:46 +0100

    Add bike team pursuit benchmark (rewriting 1153) (#1387)
    * New problem function: Team Pursuit Track Cycling
    
    * submitting changes for team pursuit cycling problem
    
    * adding cycling experiment
    
    * fixing linting issues
    
    * headers
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Update nevergrad/benchmark/experiments.py
    
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>
    
    * testcycling
    
    * Bike2 (#1390)
    
    * testcycling
    
    * fix
    
    * fix
    
    * Update nevergrad/benchmark/experiments.py
    
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>
    
    * Update nevergrad/benchmark/experiments.py
    
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>
    
    * Update nevergrad/functions/cycling/cycling.py
    
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>
    
    * Update nevergrad/functions/cycling/__init__.py
    
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>
    
    * fix
    
    Co-authored-by: Ryan Kroon <rkroon19@gmail.com>
    Co-authored-by: Jérémy Rapin <jrapin.github@gmail.com>

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -32,6 +32,7 @@ from nevergrad.functions import rl
 from nevergrad.functions.games import game
 from nevergrad.functions import iohprofiler
 from nevergrad.functions import helpers
+from nevergrad.functions.cycling import Cycling
 from .xpbase import Experiment as Experiment
 from .xpbase import create_seed_generator
 from .xpbase import registry as registry  # noqa
@@ -1978,3 +1979,16 @@ def unit_commitment(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
                     xp = Experiment(func, algo, budget, num_workers=1, seed=next(seedg))
                     if not xp.is_incoherent:
                         yield xp
+
+
+def team_cycling(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
+    """Experiment to optimise team pursuit track cycling problem."""
+    seedg = create_seed_generator(seed)
+    optims = ["NGOpt10", "CMA", "DE"]
+    funcs = [Cycling(num) for num in [30, 31, 61, 22, 23, 45]]
+    for function in funcs:
+        for budget in [3000]:
+            for optim in optims:
+                xp = Experiment(function, optim, budget=budget, num_workers=10, seed=next(seedg))
+                if not xp.is_incoherent:
+                    yield xp
