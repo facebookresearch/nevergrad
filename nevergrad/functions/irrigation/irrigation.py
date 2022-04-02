41be911d0848bc85f1c0dfc8ec13f1d349b70caf
Olivier Teytaud <oteytaud@fb.com>
2022-04-02 22:18:32 +0200

    fix

--- nevergrad/functions/irrigation/irrigation.py
+++ nevergrad/functions/irrigation/irrigation.py
@@ -31,24 +31,44 @@ from pcse.base import ParameterProvider
 class Irrigation(ArrayExperimentFunction):
     def __init__(self, symmetry: int) -> None:
         data_dir = Path(__file__).with_name("data")
-        #urllib.request.urlretrieve(
+        # urllib.request.urlretrieve(
         #    "https://raw.githubusercontent.com/ajwdewit/pcse_notebooks/master/data/soil/ec3.soil",
         #    str(data_dir) + "/soil/ec3.soil",
-        #)
+        # )
         self.soil = CABOFileReader(os.path.join(data_dir, "soil", "ec3.soil"))
         param = ng.p.Array(shape=(8,), lower=(0.0), upper=(1.0)).set_name("irrigation8")
         super().__init__(self.leaf_area_index, parametrization=param, symmetry=symmetry)
         if os.environ.get("CIRCLECI", False):
             raise ng.errors.UnsupportedExperiment("No HTTP request in CircleCI")
-        known_longitudes = {'Saint-Leger-Bridereix': 1.5887348, 'Dun-Le-Palestel': 1.6641173, 'Kolkata':
-        88.35769124388872, 'Antananarivo': 47.5255809, 'Santiago': -70.6504502, 'Lome': 1.215829, 'Cairo': 31.2357257,
-        'Ouagadougou': -1.5270944, 'Yamoussoukro': -5.273263, 'Yaounde': 11.5213344, 'Kiev': 30.5241361}
-        known_latitudes = {'Saint-Leger-Bridereix': 46.2861759, 'Dun-Le-Palestel': 46.3052049, 'Kolkata': 22.5414185,
-        'Antananarivo': -18.9100122, 'Santiago': -33.4377756, 'Lome': 6.130419, 'Cairo': 30.0443879, 'Ouagadougou':
-        12.3681873, 'Yamoussoukro': 6.809107, 'Yaounde': 3.8689867, 'Kiev': 50.4500336}
+        known_longitudes = {
+            "Saint-Leger-Bridereix": 1.5887348,
+            "Dun-Le-Palestel": 1.6641173,
+            "Kolkata": 88.35769124388872,
+            "Antananarivo": 47.5255809,
+            "Santiago": -70.6504502,
+            "Lome": 1.215829,
+            "Cairo": 31.2357257,
+            "Ouagadougou": -1.5270944,
+            "Yamoussoukro": -5.273263,
+            "Yaounde": 11.5213344,
+            "Kiev": 30.5241361,
+        }
+        known_latitudes = {
+            "Saint-Leger-Bridereix": 46.2861759,
+            "Dun-Le-Palestel": 46.3052049,
+            "Kolkata": 22.5414185,
+            "Antananarivo": -18.9100122,
+            "Santiago": -33.4377756,
+            "Lome": 6.130419,
+            "Cairo": 30.0443879,
+            "Ouagadougou": 12.3681873,
+            "Yamoussoukro": 6.809107,
+            "Yaounde": 3.8689867,
+            "Kiev": 50.4500336,
+        }
         self.cropd = YAMLCropDataProvider()
         for k in range(1000):
-            self.address = np.random.RandomState(symmetry+3*k).choice(
+            self.address = np.random.RandomState(symmetry + 3 * k).choice(
                 [
                     "Saint-Leger-Bridereix",
                     "Dun-Le-Palestel",
@@ -64,10 +84,13 @@ class Irrigation(ArrayExperimentFunction):
                 ]
             )
             if self.address in known_latitudes and self.address in known_longitudes:
-                self.weatherdataprovider = NASAPowerWeatherDataProvider(latitude=known_latitudes[self.address], longitude=known_longitudes[self.address])
-            else:           
+                self.weatherdataprovider = NASAPowerWeatherDataProvider(
+                    latitude=known_latitudes[self.address], longitude=known_longitudes[self.address]
+                )
+            else:
                 assert False
                 from geopy.geocoders import Nominatim
+
                 geolocator = Nominatim(user_agent="NG/PCSE")
                 self.location = geolocator.geocode(self.address)
                 self.weatherdataprovider = NASAPowerWeatherDataProvider(
@@ -79,17 +102,16 @@ class Irrigation(ArrayExperimentFunction):
                 break
         print(f"we work on {self.cropname} with variety {self.cropvariety} in {self.address}.")
 
-
     def set_data(self, symmetry: int, k: int):
         crop_types = [c for c in self.cropd.crop_types if "obacco" not in c]
-        self.cropname = np.random.RandomState(symmetry+3*k+1).choice(crop_types)
-        self.cropvariety = np.random.RandomState(symmetry+3*k+2).choice(list(self.cropd.get_crops_varieties()[self.cropname])
+        self.cropname = np.random.RandomState(symmetry + 3 * k + 1).choice(crop_types)
+        self.cropvariety = np.random.RandomState(symmetry + 3 * k + 2).choice(
+            list(self.cropd.get_crops_varieties()[self.cropname])
         )
         # We check if the problem is challenging.
         print(f"testing {symmetry}: {k} {self.address} {self.cropvariety}")
         site = WOFOST72SiteDataProvider(WAV=100, CO2=360)
         self.parameterprovider = ParameterProvider(soildata=self.soil, cropdata=self.cropd, sitedata=site)
-
 
     def leaf_area_index(self, x: np.ndarray):
         d0 = int(1.01 + 29.98 * x[0])
@@ -128,10 +150,10 @@ class Irrigation(ArrayExperimentFunction):
             wofost.run_till_terminate()
         except Exception as e:
             return float("inf")
-            #assert (
+            # assert (
             #    False
-            #), f"Problem!\n Dates: {d0} {d1} {d2} {d3},\n amounts: {a0}, {a1}, {a2}, {a3}\n  ({e}).\n"
-            #raise e
+            # ), f"Problem!\n Dates: {d0} {d1} {d2} {d3},\n amounts: {a0}, {a1}, {a2}, {a3}\n  ({e}).\n"
+            # raise e
 
         output = wofost.get_output()
         df = pd.DataFrame(output).set_index("day")
