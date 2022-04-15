51bd4a0283c5cfb22e983739b2b2f5b70ed778a8
Teytaud <olivier.teytaud@gmail.com>
2022-04-15 13:35:48 +0200

    fix (#1428)

--- nevergrad/optimization/optimizerlib.py
+++ nevergrad/optimization/optimizerlib.py
@@ -418,8 +418,11 @@ class _CMA(base.Optimizer):
         super().__init__(parametrization, budget=budget, num_workers=num_workers)
         self._config = ParametrizedCMA() if config is None else config
         pop = self._config.popsize
-        self._popsize = max(num_workers, 4 + int(3 * np.log(self.dimension))) if pop is None else pop
-        # internal attributes
+        self._popsize = (
+            max(num_workers, 4 + int(self._config.popsize_factor * np.log(self.dimension)))
+            if pop is None
+            else pop
+        )  # internal attributes
         self._to_be_asked: tp.Deque[np.ndarray] = deque()
         self._to_be_told: tp.List[p.Parameter] = []
         self._num_spawners = self._popsize // 2  # experimental, for visualization
@@ -531,6 +534,8 @@ class ParametrizedCMA(base.ConfiguredOptimizer):
     popsize: Optional[int] = None
         population size, should be n * self.num_workers for int n >= 1.
         default is max(self.num_workers, 4 + int(3 * np.log(self.dimension)))
+    popsize_factor: float = 3.
+        factor in the formula for computing the population size
     diagonal: bool
         use the diagonal version of CMA (advised in big dimension)
     high_speed: bool
