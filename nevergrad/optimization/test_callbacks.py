3c608b244e787b6a3b9c6d2bca471df3196ad881
Olivier Teytaud <oteytaud@fb.com>
2022-04-21 16:00:39 +0200

    fk

--- nevergrad/optimization/test_callbacks.py
+++ nevergrad/optimization/test_callbacks.py
@@ -38,7 +38,7 @@ def test_log_parameters(tmp_path: Path) -> None:
     assert isinstance(logs[-1]["1"], float)
     assert len(logs[-1]) == 33
     logs = logger.load_flattened(max_list_elements=2)
-    assert len(logs[-1]) == 28
+    assert len(logs[-1]) == 29
     # deletion
     logger = callbacks.ParametersLogger(filepath, append=False)
     assert not logger.load()
