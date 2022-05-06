4682e18252d0aae2b60b817d1750fa42939dc19c
Olivier Teytaud <oteytaud@fb.com>
2022-05-06 11:30:15 +0200

    fix

---
+++ nevergrad/functions/gym/tuple_gym_env.py
@@ -0,0 +1,49 @@
+import gym
+import numpy as np
+from gym.spaces import Box, Tuple, MultiDiscrete
+
+
+class TupleActionSpace(gym.Env):
+    """
+    Environment with tuple of discrete spaces at each time step.
+    """
+
+    def __init__(self):
+
+        super().__init__()
+        self.current_step = 0
+        self.num_cells = 71
+        self.action_space = Tuple((MultiDiscrete(self.num_cells * [2]), MultiDiscrete(self.num_cells * [6])))
+        self.horizon = 168
+        self._reward = 0.0
+
+        self.observation_space = Box(
+            low=np.array([0, 0, 0]),
+            high=np.array([1, 1, 1]),
+            dtype=np.float16,
+        )
+
+    def _next_observation(self):
+
+        return (0.5, 0.5, 0.5)
+
+    def reward(
+        self,
+    ) -> float:
+        return self._reward
+
+    def _take_action(self, action):
+        assert len(action) == 2
+        assert len(action[0]) == self.num_cells
+        self._reward = np.sum(action[0]) - np.sum(action[1])
+        return self._reward
+
+    def reset(self):
+        return self._next_observation()
+
+    def step(self, action):
+        # Execute one time step within the environment
+        self._take_action(action)
+        obs = self._next_observation()
+        self.current_step += 1
+        return obs, self.reward(), self.current_step == self.horizon, {}
