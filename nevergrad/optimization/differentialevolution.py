478c58846190ae617fc53f8eceb6e59a0228026f
Patrick Lenihan <83040764+Game4Move78@users.noreply.github.com>
2022-07-15 17:27:18 +0100

    Mention in docstring that popsize is clipped

--- nevergrad/optimization/differentialevolution.py
+++ nevergrad/optimization/differentialevolution.py
@@ -278,8 +278,8 @@ class DifferentialEvolution(base.ConfiguredOptimizer):
     F2: float
         differential weight #2
     popsize: int, "standard", "dimension", "large"
-        size of the population to use. "standard" is max(num_workers, 30), "dimension" max(num_workers, 30, dimension +1)
-        and "large" max(num_workers, 30, 7 * dimension).
+        size of the population to use. "standard" is 30, "dimension" max(30, dimension +1) and "large" 
+        max(30, 7 * dimension). Set to be at least num_workers so that base vectors are distinct.
     multiobjective_adaptation: bool
         Automatically adapts to handle multiobjective case.  This is a very basic **experimental** version,
         activated by default because the non-multiobjective implementation is performing very badly.
