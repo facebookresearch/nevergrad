cfebd90dffd68e1c6d54c15668e95a51c253b87d
Teytaud <olivier.teytaud@gmail.com>
2022-07-22 10:42:58 +0200

    Maybe MixDeterministicRL.no_parallelization = True

--- nevergrad/optimization/experimentalvariants.py
+++ nevergrad/optimization/experimentalvariants.py
@@ -357,6 +357,8 @@ SmoothAdaptiveDiscreteOnePlusOne = ParametrizedOnePlusOne(smoother=True, mutatio
 MixDeterministicRL = ConfPortfolio(optimizers=[DiagonalCMA, PSO, GeneticDE]).set_name(
     "MixDeterministicRL", register=True
 )
+MixDeterministicRL.no_parallelization = True
+
 SpecialRL = Chaining([MixDeterministicRL, TBPSA], ["half"]).set_name("SpecialRL", register=True)
 NoisyRL1 = Chaining([MixDeterministicRL, NoisyOnePlusOne], ["half"]).set_name("NoisyRL1", register=True)
 NoisyRL2 = Chaining(
