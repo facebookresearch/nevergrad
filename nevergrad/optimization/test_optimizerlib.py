e842aca2619ecec3450b9a38df2e9e929653089e
Olivier Teytaud <oteytaud@fb.com>
2022-12-12 17:21:54 +0100

    fix

--- nevergrad/optimization/test_optimizerlib.py
+++ nevergrad/optimization/test_optimizerlib.py
@@ -737,9 +737,11 @@ def test_ngopt_selection(
                 # assert choice == "MetaTuneRecentering"
                 assert "MetaTuneRecentering" in optim_string
             if num_workers > 1:
-                # assert choice not in ["SQP", "Cobyla"]
-                assert "SQP" not in optim_string and "Cobyla" not in optim_string
-        assert opt._info()["sub-optim"] in optim_string
+               assert choice not in ["SQP", "Cobyla"]
+        if "CMA" not in choice:
+            assert choice == opt._info()["sub-optim"]
+        else:
+            assert choice in opt._info()["sub-optim"]
 
 
 def test_bo_ordering() -> None:
