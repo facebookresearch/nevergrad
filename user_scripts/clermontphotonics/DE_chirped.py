e5563dbdd64d6950224a9b86aaf75263f412d402
hurlements <antoine.moreau@uca.fr>
2023-03-27 15:58:11 +0200

    Script chirped DE adapté (on garde 1/100 de la convergence).

--- user_scripts/clermontphotonics/DE_chirped.py
+++ user_scripts/clermontphotonics/DE_chirped.py
@@ -9,9 +9,9 @@ from joblib import Parallel, delayed
 def launch_optim(n_couches):
     # A pour Antoine.
     algo = "DE"
-    function = "chirped[400-650.50]"
-    budget = 1000000
-    nb_runs = 40
+    function = "chirpedHC[400-650.50]"
+    budget = 100000
+    nb_runs = 30
 
     X_min=np.hstack(20*np.ones(n_couches))
     X_max=np.hstack(250*np.ones(n_couches))
@@ -21,7 +21,7 @@ def launch_optim(n_couches):
     for k in range(nb_runs):
         depop = 30
         [best,convergence,recom] = algos.DEvol(photonics.chirped,budget,X_min,X_max,depop)
-        results.append([best,convergence[::100]]+convergence[-1])
+        results.append([best]+convergence[::100]+[convergence[-1]])
         print(f"Run {k} with {algo} on {function} with {n_couches} layers")
 
     file_name = f"ResA/{function}_{algo}_{n_couches}_{budget}.npy"
@@ -30,4 +30,4 @@ def launch_optim(n_couches):
 
 # Relire les données
 
-Parallel(n_jobs = 24)(delayed(launch_optim)(n_couches) for n_couches in [20,30,40,50,60,70,80])
+Parallel(n_jobs = 24)(delayed(launch_optim)(n_couches) for n_couches in [20,30,40,50,60])
