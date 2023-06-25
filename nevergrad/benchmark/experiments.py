8f006188ba8e148f5b0eb006e6e3cca6cb0c1cbb
Teytaud <olivier.teytaud@gmail.com>
2023-06-25 14:35:22 +0200

    topology optimization artificial benchmark (#1525)
    * to
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -24,6 +24,7 @@ from nevergrad.functions import images as imagesxp
 from nevergrad.functions.powersystems import PowerSystem
 from nevergrad.functions.ac import NgAquacrop
 from nevergrad.functions.stsp import STSP
+from nevergrad.functions.topology_optimization import TO
 from nevergrad.functions.rocket import Rocket
 from nevergrad.functions.mixsimulator import OptimizeMix
 from nevergrad.functions.unitcommitment import UnitCommitmentProblem
@@ -1620,6 +1621,30 @@ def olympus_emulators(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
                         xp = Experiment(fu, algo, budget, num_workers=num_workers, seed=next(seedg))
                         if not xp.is_incoherent:
                             yield xp
+
+
+@registry.register
+def topology_optimization(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
+    seedg = create_seed_generator(seed)
+    funcs = [TO(i) for i in [10, 20, 30, 40]]
+    optims = ["CMA", "GeneticDE", "TwoPointsDE", "VoronoiDE", "DE", "PSO", "RandomSearch", "OnePlusOne"]
+    for budget in [10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240, 20480, 40960]:
+        for optim in optims:
+            for f in funcs:
+                for nw in [1, 30]:
+                    yield Experiment(f, optim, budget, num_workers=nw, seed=next(seedg))
+
+
+@registry.register
+def sequential_topology_optimization(seed: tp.Optional[int] = None) -> tp.Iterator[Experiment]:
+    seedg = create_seed_generator(seed)
+    funcs = [TO(i) for i in [10, 20, 30, 40]]
+    optims = ["CMA", "GeneticDE", "TwoPointsDE", "VoronoiDE", "DE", "PSO", "RandomSearch", "OnePlusOne"]
+    for budget in [10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240, 20480, 40960]:
+        for optim in optims:
+            for f in funcs:
+                for nw in [1, 30]:
+                    yield Experiment(f, optim, budget, num_workers=nw, seed=next(seedg))
 
 
 @registry.register
