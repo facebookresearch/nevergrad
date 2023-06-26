03098e592131333a954bba080cbf8317d5ee08c8
Olivier Teytaud <oteytaud@fb.com>
2023-06-26 17:14:53 +0200

    fix

--- scripts/latexize.sh
+++ scripts/latexize.sh
@@ -3,8 +3,8 @@
 cat scripts/tex/beginning.tex
 for u in *plots/
 do
-echo "\\subsection{`echo $u | sed 's/_plots.$//g'`}" | sed 's/_/ /g'
-ls ${u}/*all.png | sed 's/.*/\\includegraphics[width=.8\\textwidth]{{&}}\\\\/g' | sed 's/aquacrop/(RW) &/g' | sed 's/rocket/(RW)&/g' | sed 's/fishing/(RW)&/g' | sed 's/MLDA/(RW)&/g' | sed 's/keras/(RW)&/g' | sed 's/mltuning/(RW)&/g'
+echo "\\subsection{`echo $u | sed 's/_plots.$//g'`}" | sed 's/_/ /g' | sed 's/aquacrop/(RW) &/g' | sed 's/rocket/(RW)&/g' | sed 's/fishing/(RW)&/g' | sed 's/MLDA/(RW)&/g' | sed 's/keras/(RW)&/g' | sed 's/mltuning/(RW)&/g'
+ls ${u}/*all.png | sed 's/.*/\\includegraphics[width=.8\\textwidth]{{&}}\\\\/g'
 done
 echo '\section{Conclusion}'
 cat scripts/tex/conclusion.tex
@@ -12,9 +12,11 @@ echo '\appendix'
 echo '\section{Competence maps}'
 for u in *plots/
 do
-echo "\\subsection{`echo $u | sed 's/_plots.$//g'`}" | sed 's/_/ /g'
-ls ${u}/comp*.pdf | sed 's/.*/\\includegraphics[width=.8\\textwidth]{{&}}\\\\/g' | sed 's/aquacrop/(RW) &/g' | sed 's/rocket/(RW)&/g' | sed 's/fishing/(RW)&/g' | sed 's/MLDA/(RW)&/g' | sed 's/keras/(RW)&/g' | sed 's/mltuning/(RW)&/g'
+echo "\\subsection{`echo $u | sed 's/_plots.$//g'`}" | sed 's/_/ /g' | sed 's/aquacrop/(RW) &/g' | sed 's/rocket/(RW)&/g' | sed 's/fishing/(RW)&/g' | sed 's/MLDA/(RW)&/g' | sed 's/keras/(RW)&/g' | sed 's/mltuning/(RW)&/g'
+ls ${u}/comp*.pdf | sed 's/.*/\\includegraphics[width=.8\\textwidth]{{&}}\\\\/g' 
 done
 cat scripts/tex/end.tex ) > dagstuhloid.tex
 cp scripts/tex/biblio.bib .
+pdflatex dagstuhloid.tex
+bibtex dagstuhloid.aux
 pdflatex dagstuhloid.tex
