054983268a8f48394ec82a5cd5004d65c1bb36fe
Olivier Teytaud <oteytaud@fb.com>
2023-06-26 17:10:31 +0200

    po

--- scripts/latexize.sh
+++ scripts/latexize.sh
@@ -3,9 +3,7 @@
 cat scripts/tex/beginning.tex
 for u in *plots/
 do
-echo "\\subsection{`echo $u | sed 's/_plots.$//g'`}" | sed 's/_/ /g' | sed 's/aquacrop/(RW) &/g' | sed 's/rocket/(RW)&/g' | sed 's/fishing/(RW)&/g' | sed 's/MLDA/(RW)&/g' | sed 's/keras/(RW)&/g' | sed 's/mltuning/(RW)&/g'
-ls ${u}/*all.png | sed 's/.*/\\includegraphics[width=.8\\textwidth]{{&}}\\\\/g'
-echo "\\subsection{`echo $u | sed 's/_plots.$//g'`}" | sed 's/_/ /g'| sed 's/powersystems/(RW)&/g' | sed 's/mixsimulator/(RW)&/g' | sed 's/olympus/(RW)&/g'
+echo "\\subsection{`echo $u | sed 's/_plots.$//g'`}" | sed 's/_/ /g'| sed 's/aquacrop/(RW) &/g' | sed 's/rocket/(RW)&/g' | sed 's/fishing/(RW)&/g' | sed 's/MLDA/(RW)&/g' | sed 's/keras/(RW)&/g' | sed 's/mltuning/(RW)&/g' | sed 's/powersystems/(RW)&/g' | sed 's/mixsimulator/(RW)&/g' | sed 's/olympus/(RW)&/g'
 ls ${u}/*all.png | sed 's/.*/\\includegraphics[width=.8\\textwidth]{{&}}\\\\/g' 
 done
 echo '\section{Conclusion}'
@@ -14,7 +12,7 @@ echo '\appendix'
 echo '\section{Competence maps}'
 for u in *plots/
 do
-echo "\\subsection{`echo $u | sed 's/_plots.$//g'`}" | sed 's/_/ /g' | sed 's/aquacrop/(RW) &/g' | sed 's/rocket/(RW)&/g' | sed 's/fishing/(RW)&/g' | sed 's/MLDA/(RW)&/g' | sed 's/keras/(RW)&/g' | sed 's/mltuning/(RW)&/g'| sed 's/aquacrop/(RW) &/g' | sed 's/rocket/(RW)&/g' | sed 's/fishing/(RW)&/g' | sed 's/MLDA/(RW)&/g' | sed 's/keras/(RW)&/g' | sed 's/mltuning/(RW)&/g' 
+echo "\\subsection{`echo $u | sed 's/_plots.$//g'`}" | sed 's/_/ /g'| sed 's/aquacrop/(RW) &/g' | sed 's/rocket/(RW)&/g' | sed 's/fishing/(RW)&/g' | sed 's/MLDA/(RW)&/g' | sed 's/keras/(RW)&/g' | sed 's/mltuning/(RW)&/g' | sed 's/powersystems/(RW)&/g' | sed 's/mixsimulator/(RW)&/g' | sed 's/olympus/(RW)&/g'
 ls ${u}/comp*.pdf | sed 's/.*/\\includegraphics[width=.8\\textwidth]{{&}}\\\\/g' 
 done
 cat scripts/tex/end.tex ) > dagstuhloid.tex
