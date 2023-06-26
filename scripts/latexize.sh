65ef657b149cf701ed6054edc13e4efc37352bcf
Olivier Teytaud <oteytaud@fb.com>
2023-06-26 15:20:17 +0200

    fix

--- scripts/latexize.sh
+++ scripts/latexize.sh
@@ -16,5 +16,5 @@ echo "\\subsection{`echo $u | sed 's/_plots.$//g'`}" | sed 's/_/ /g'
 ls ${u}/comp*.pdf | sed 's/.*/\\includegraphics[width=.8\\textwidth]{{&}}\\\\/g' | sed 's/aquacrop/(RW) &/g' | sed 's/rocket/(RW)&/g' | sed 's/fishing/(RW)&/g' | sed 's/MLDA/(RW)&/g'
 done
 cat scripts/tex/end.tex ) > dagstuhloid.tex
-
+cp scripts/tex/biblio.bib .
 pdflatex dagstuhloid.tex
