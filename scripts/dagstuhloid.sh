3093b61596fe9042e65938b0f8c7048e12396a2b
Olivier Teytaud <oteytaud@fb.com>
2023-06-30 19:03:36 +0200

    fix

--- scripts/dagstuhloid.sh
+++ scripts/dagstuhloid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=dagstuhloid
-#SBATCH --output=dagstuhloid_%j.out
-#SBATCH --error=dagstuhloid_%j.err
+#SBATCH --output=dagstuhloid_%A_%a.out
+#SBATCH --error=dagstuhloid_%A_%a.err
 #SBATCH --time=72:00:00
 #SBATCH --partition=scavenge
 #SBATCH --nodes=1
@@ -9,8 +9,7 @@
 #SBATCH -a 0-74
 
 
-tasks=(keras_tuning mltuning naivemltuning seq_keras_tuning naive_seq_keras_tuning oneshot_mltuning seq_mltuning nano_seq_mltuning nano_naive_seq_mltuning naive_seq_mltuning bonnans yabbob reduced_yahdlbbbob yaconstrainedbbob yapenbbob yamegapenhdbbob yaonepenbigbbob yamegapenbigbbob yamegapenboxbbob yamegapenbbob yamegapenboundedbbob yapensmallbbob yapenboundedbbob yapennoisybbob yapenparabbob yapenboxbbob yaonepenbbob yaonepensmallbbob yaonepenboundedbbob yaonepennoisybbob yaonepenparabbob yaonepenboxbbob yahdnoisybbob yabigbbob yatuningbbob yatinybbob yasmallbbob yahdbbob yaparabbob yanoisybbob yaboundedbbob yaboxbbob pbbob boundedpbbob spsa_benchmark aquacrop_fao fishing rocket mono_rocket mixsimulator control_problem neuro_control_problem olympus_surfaces olympus_emulators simple_tsp complex_tsp sequential_fastgames powersystems mldakmeans double_o_seven multiobjective_example multiobjective_example_hd multiobjective_example_many_hd multiobjective_example_many photonics photonics2 small_photonics small_photonics2 pbo_reduced_suite causal_similarity unit_commitment team_cycling topology_optimization sequential_topology_optimization)
-
+tasks=(veryseq_keras_tuning naive_veryseq_keras_tuning nano_veryseq_mltuning nano_naive_veryseq_mltuning keras_tuning mltuning naivemltuning seq_keras_tuning naive_seq_keras_tuning oneshot_mltuning seq_mltuning nano_seq_mltuning nano_naive_seq_mltuning naive_seq_mltuning bonnans yabbob reduced_yahdlbbbob yaconstrainedbbob yapenbbob yamegapenhdbbob yaonepenbigbbob yamegapenbigbbob yamegapenboxbbob yamegapenbbob yamegapenboundedbbob yapensmallbbob yapenboundedbbob yapennoisybbob yapenparabbob yapenboxbbob yaonepenbbob yaonepensmallbbob yaonepenboundedbbob yaonepennoisybbob yaonepenparabbob yaonepenboxbbob yahdnoisybbob yabigbbob yatuningbbob yatinybbob yasmallbbob yahdbbob yaparabbob yanoisybbob yaboundedbbob yaboxbbob pbbob boundedpbbob spsa_benchmark aquacrop_fao fishing rocket mono_rocket mixsimulator control_problem neuro_control_problem olympus_surfaces olympus_emulators simple_tsp complex_tsp sequential_fastgames powersystems mldakmeans double_o_seven multiobjective_example multiobjective_example_hd multiobjective_example_many_hd multiobjective_example_many photonics photonics2 small_photonics small_photonics2 pbo_reduced_suite causal_similarity unit_commitment team_cycling topology_optimization sequential_topology_optimization ultrasmall_photonics ultrasmall_photonics2) 
 # SLURM_ARRAY_TASK_ID=154 # comment in for testing
 
 task=${tasks[SLURM_ARRAY_TASK_ID]}
