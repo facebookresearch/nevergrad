c8ca3c8a5a323f3cd8c22506e9faae27ae4247e0
Olivier Teytaud <oteytaud@fb.com>
2023-09-19 18:04:47 +0200

    po

--- nevergrad/optimization/optimizerlib.py
+++ nevergrad/optimization/optimizerlib.py
@@ -3886,9 +3886,7 @@ class NgIoh4(NGOptBase):
 class NgIohRW(NgIoh4):
     def _select_optimizer_cls(self) -> base.OptCls:
         if self.fully_continuous and not self.has_noise and self.budget >= 12 * self.dimension:  # type: ignore
-            return ConfPortfolio(
-                optimizers=[QODE, PSO, super()._select_optimizer_cls()], warmup_ratio=0.33
-            )
+            return ConfPortfolio(optimizers=[QODE, PSO, super()._select_optimizer_cls()], warmup_ratio=0.33)
         else:
             return super()._select_optimizer_cls()
 
