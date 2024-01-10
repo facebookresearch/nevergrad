b5eb54c4d4bd28d45eea50f2868f28b621f41fa6
Olivier Teytaud <oteytaud@fb.com>
2024-01-10 21:06:42 +0200

    fix

--- nevergrad/benchmark/experiments.py
+++ nevergrad/benchmark/experiments.py
@@ -56,7 +56,7 @@ def refactor_optims(x: tp.List[tp.Any]) -> tp.List[tp.Any]:  # type: ignore
     #    return [np.random.choice(["ChainCMASQP", "BFGSCMAPlus", "SQPCMAPlus", "ChainMetaModelSQP", "QNDE", "NGOptRW", "MultiCobyla", "LBFGSB", "NLOPT-LN-COBYLA", "MultiCobylaPlus", "RCobyla", "PymooBIPOP", "MultiSQP", "MultiBFGS", "LogBFGSCMAPlus", "RBFGS", "SQP", "LogSQPCMAPlus", "MultiSQPPlus", "RSQP", "MultiBFGSPlus", "BFGS", "LogMultiBFGS", "SqrtMultiBFGS", "F3SQPCMA", "SqrtSQPCMAPlus", "LogMultiBFGSPlus", "SqrtBFGSCMAPlus", "TinySQP", "MicroSQP", "SqrtMultiBFGSPlus" ])]
     # if np.random.randn() < 0.0:
     #    return ["NgIoh8"]
-    if True:  # np.random.randn() < 0.0:
+    if False:  # np.random.randn() < 0.0:
         return list(
             np.random.choice(
                 [
@@ -80,7 +80,7 @@ def refactor_optims(x: tp.List[tp.Any]) -> tp.List[tp.Any]:  # type: ignore
             )
         )
     return list(
-        np.random.choice(
+        (
             [
                 "CMandAS2",
                 "SQOPSO",
@@ -498,7 +498,7 @@ def refactor_optims(x: tp.List[tp.Any]) -> tp.List[tp.Any]:  # type: ignore
     benchmark = str(inspect.stack()[1].function)
     if benchmark in algos and "tunin" in benchmark and np.random.randint(2) > 0 and False:
         return algos[benchmark][:5]
-
+    return ["NGOpt", "NgIoh17"]
     # Here, we pseudo-randomly draw one optim in the provided list,
     # depending on the host (so that each host is using the same optim).
     #    list_optims = x
