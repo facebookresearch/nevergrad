a5222c9aa1a1b10236c5597a8eb1e92f9737fef9
Olivier Teytaud <oteytaud@fb.com>
2024-03-25 22:53:13 +0100

    fix

--- nevergrad/optimization/test_optimizerlib.py
+++ nevergrad/optimization/test_optimizerlib.py
@@ -51,7 +51,7 @@ def long_name(s: str):
         return False
     if "DS" in s or "AX" in s or "BO" in s or any(x in s for x in [str(i) for i in range(10)]):
         return True
-    return len(s.replace("DiscreteOnePlusOne", "D1+1").replace("Tuned", "")) > 2 and os.environ.get(
+    return len(s.replace("DiscreteOnePlusOne", "D1+1").replace("Tuned", "")) > 0 and os.environ.get(
         "CIRCLECI", False
     )
 
