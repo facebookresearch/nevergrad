755aa05d4b9eddfe947fb91e752ca1702e1c8f16
Olivier Teytaud <oteytaud@fb.com>
2024-07-02 10:04:54 +0200

    ng_coevol

---
+++ examples/ng_coevol.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+#SBATCH --job-name=ngcoevol
+#SBATCH --output=ngcoevol_%a_%A.out
+#SBATCH --error=ngcoevol_%a_%A.err
+#SBATCH --time=72:00:00
+#SBATCH --partition=scavenge
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=80
+#SBATCH -a 0-10
+
+for a in `seq 120`
+do
+(
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+python examples/ng_coevol.py
+
+) &
+done
+
+
+wait
