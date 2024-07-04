feb6b76e042c5ae3881ddd09282d36e72fac9bc8
Olivier Teytaud <oteytaud@fb.com>
2024-07-04 10:14:04 +0200

    ceviche

--- scripts/ceviche.sh
+++ scripts/ceviche.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=ceviche
-#SBATCH --output=ceviche_%A_%a.out
-#SBATCH --error=ceviche_%A_%a.err
+#SBATCH --job-name=dagstuhloid
+#SBATCH --output=dagstuhloid_%A_%a.out
+#SBATCH --error=dagstuhloid_%A_%a.err
 #SBATCH --time=72:00:00
-#SBATCH --partition=learnlab
+#SBATCH --partition=scavenge
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=70
-#SBATCH -a 0-250
+#SBATCH -a 0-73
 
-eval "$(conda shell.bash hook)"
-conda activate miniceviche
 
-#task=${tasks[SLURM_ARRAY_TASK_ID]}
-if (( RANDOM % 2 )); then
-  task=multi_ceviche
-else 
-  task=multi_ceviche_c0
-fi
+task=multi_ceviche_c0
 echo task attribution $SLURM_ARRAY_TASK_ID $task
 echo Keras/TF versions:
 pip show keras tensorflow tensorflow-estimator
@@ -27,7 +20,14 @@ echo Starting at
 date
 # num_workers is the number of processes. Maybe use a bit more than the number of cores at the line "cpus-per-task"
 # above.
-python -m nevergrad.benchmark $task --num_workers=70 2>&1 | tail -n 50
+python -m nevergrad.benchmark $task --num_workers=73 2>&1 | tail -n 50
+python -m nevergrad.benchmark $task --num_workers=73 2>&1 | tail -n 50
+python -m nevergrad.benchmark $task --num_workers=73 2>&1 | tail -n 50
+python -m nevergrad.benchmark $task --num_workers=73 2>&1 | tail -n 50
+python -m nevergrad.benchmark $task --num_workers=73 2>&1 | tail -n 50
+python -m nevergrad.benchmark $task --num_workers=73 2>&1 | tail -n 50
+python -m nevergrad.benchmark $task --num_workers=73 2>&1 | tail -n 50
+python -m nevergrad.benchmark $task --num_workers=73 2>&1 | tail -n 50
 echo task over $SLURM_ARRAY_TASK_ID $task
 echo Finishing at
 date
