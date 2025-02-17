c57062094dde962340009b75684e59832cdcee16
Olivier Teytaud <oteytaud@fb.com>
2025-02-17 14:21:42 +0200

    fix

--- nevergrad/functions/photonics/gambas.py
+++ nevergrad/functions/photonics/gambas.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+import math
 import numpy as np
 import typing as tp
 from .mode_converter import mode_converter
