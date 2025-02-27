941da84438339a9e82416bd556788a0cd6f42b14
Olivier Teytaud <oteytaud@fb.com>
2025-02-27 13:27:49 +0100

    pixel

--- examples/check_metamodel.py
+++ examples/check_metamodel.py
@@ -1,3 +1,8 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath("."))
+
+from joblib import Parallel, delayed
 import nevergrad as ng
 import numpy as np
 
@@ -7,15 +12,14 @@ print(ng.__file__)
 
 for kbudget in [1, 5, 10]:
  print("kbudget =", kbudget)
- for dim in [1,2]:
+ for dim in [2,1,3]:
     print("Experiment in dimension ", dim)
     Nk=1   # we might play with greater values later
-    N = 15
+    N = 30
     #budget = 10*((N**dim)**2)
-    budget = int(1*((N**1)**2)*kbudget)
+    budget = int(1*((N**1)**1)*kbudget)
     print(dim, N, budget)
     
-    domain = ng.p.Array(shape=[N]*dim, lower=0., upper=1.)
     
     Y = []
     for k in range(Nk):
@@ -30,11 +34,15 @@ for kbudget in [1, 5, 10]:
 
        return np.min([subf(x,y) for y in Y])
     
-    num_manips = 34
+    num_manips = 9
     #for optim in ["LognormalDiscreteOnePlusOne", "RFMetaModelLogNormal", "NeuralMetaModelLogNormal"]:
-    for optim in ["OnePlusOne", "MetaModelOnePlusOne", "ImageMetaModelOnePlusOne", "CMASL", "CLengler"]:
+    for optim in ["VoxelizeMetaModelOnePlusOne", "OnePlusOne", "MetaModelOnePlusOne", "ImageMetaModelOnePlusOne", "CMASL", "CLengler"]:
        loss = [] 
-       for k in range(num_manips):
-         opt = ng.optimizers.registry[optim](domain, budget).minimize(f).value
-         loss += [np.log(f(opt))]
+       #for k in range(num_manips):
+       #  opt = ng.optimizers.registry[optim](domain, budget).minimize(f).value
+       #  loss += [np.log(f(opt))]
+       def manip():
+           domain = ng.p.Array(shape=[N]*dim, lower=0., upper=1.)
+           return ng.optimizers.registry[optim](domain, budget).minimize(f).value
+       loss = Parallel(n_jobs=1)(delayed(manip)() for i in range(num_manips))
        print(optim, "==>",  np.average(loss), "+-", np.std(loss)/np.sqrt(num_manips-1))
