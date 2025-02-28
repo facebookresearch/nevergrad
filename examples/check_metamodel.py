d4871f8c1255b45615d51d89625c34bb33156b1b
Teytaud <olivier.teytaud@gmail.com>
2025-02-28 09:36:44 +0100

    the pixel/voxel PR (#1679)
    * pixel
    
    * fix
    
    * cleaner denis pr (#1675)
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Denisprclean (#1677)
    
    * improved converter, added bender
    
    * improved converter, added bender
    
    * clean
    
    * fix_unused
    
    * black
    
    ---------
    
    Co-authored-by: Denis Langevin <68906630+Milloupe@users.noreply.github.com>
    
    * pix
    
    * pixel
    
    * fix
    
    * fixpixel
    
    * fix
    
    * pix
    
    * pix
    
    ---------
    
    Co-authored-by: Denis Langevin <68906630+Milloupe@users.noreply.github.com>

--- examples/check_metamodel.py
+++ examples/check_metamodel.py
@@ -1,3 +1,9 @@
+import os
+import sys
+import time
+sys.path.insert(0, os.path.abspath("."))
+
+from joblib import Parallel, delayed
 import nevergrad as ng
 import numpy as np
 
@@ -5,17 +11,16 @@ import numpy as np
 print(ng.__file__)
 
 
-for kbudget in [1, 5, 10]:
+for kbudget in [1, 5, 10, 25, 40]:
  print("kbudget =", kbudget)
- for dim in [1,2]:
+ for dim in [2]:
     print("Experiment in dimension ", dim)
     Nk=1   # we might play with greater values later
-    N = 15
+    N = 30
     #budget = 10*((N**dim)**2)
-    budget = int(1*((N**1)**2)*kbudget)
+    budget = int(1*((N**1)**1)*kbudget)
     print(dim, N, budget)
     
-    domain = ng.p.Array(shape=[N]*dim, lower=0., upper=1.)
     
     Y = []
     for k in range(Nk):
@@ -30,11 +35,16 @@ for kbudget in [1, 5, 10]:
 
        return np.min([subf(x,y) for y in Y])
     
-    num_manips = 34
+    num_manips = 80 
     #for optim in ["LognormalDiscreteOnePlusOne", "RFMetaModelLogNormal", "NeuralMetaModelLogNormal"]:
-    for optim in ["OnePlusOne", "MetaModelOnePlusOne", "ImageMetaModelOnePlusOne", "CMASL", "CLengler"]:
+    for optim in ["DiscreteLenglerOnePlusOne", "LognormalDiscreteOnePlusOne", "OnePlusOne", "CMASL", "CLengler", "VoxelizeMetaModelOnePlusOne", "MetaModelOnePlusOne", "ImageMetaModelOnePlusOne", "DiagonalCMA", "PSO", "DE", "CMA"]:
        loss = [] 
-       for k in range(num_manips):
-         opt = ng.optimizers.registry[optim](domain, budget).minimize(f).value
-         loss += [np.log(f(opt))]
-       print(optim, "==>",  np.average(loss), "+-", np.std(loss)/np.sqrt(num_manips-1))
+       #for k in range(num_manips):
+       #  opt = ng.optimizers.registry[optim](domain, budget).minimize(f).value
+       #  loss += [np.log(f(opt))]
+       def manip():
+           domain = ng.p.Array(shape=[N]*dim, lower=0., upper=1.)
+           return ng.optimizers.registry[optim](domain, budget).minimize(f).value
+       t0 = time.time()
+       loss = Parallel(n_jobs=10)(delayed(manip)() for i in range(num_manips))
+       print(optim, "==>",  np.average(loss), "+-", np.std(loss)/np.sqrt(num_manips-1), "   t=", time.time()-t0, flush=True)
